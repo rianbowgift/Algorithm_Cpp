4496b01b1a0d249fe39fb03ec2da6c07a470d292
YakultLover <rianbowgift@gmail.com>
2022-03-21 18:38:50 +0900

    간단하게 풀수있지만, 분류상 dp므로 dp로 품. dp공부보단 C++문법공부를 다양하게할수있어서 좋았음

--- Algorithm_Cpp/Baekjoon/DP/BJ_17202.cpp
+++ Algorithm_Cpp/Baekjoon/DP/BJ_17202.cpp
@@ -2,30 +2,40 @@
 #include<string>
 using namespace std;
 
-int sum[11];
-int main() {
+int sum[20];
+int BJ_17202() {
 
-	int a, b;
+	string a, b;
 	string a_b;
+	string a_b_temp;
 	cin >> a >> b;
-
-	for (int i = 0; i < 10; i++) {
+	
+	for (int i = 0; i < 10; i++) {	//DP를위한 배열초기화. 각숫자에맞는 값이 입력된다
 		sum[i] = i;
+		sum[i + 10] = i;
 	}
-	sum[10] = 0;
-
 	
-	while (a != 0) {
-		a_b += to_string(a % 10);
-		a_b += to_string(b % 10);
-		a /= 10;
-		b /= 10;
-		break;
-	}
-
-	cout << a_b;
 	
 
+	for (int i = 0; i < 8; i++) {	//번호 2개를 1개로 병합
+		a_b += a.at(i);
+		a_b += b.at(i);
+		
+	}
+
+
+
+
+	for (int j = 15; j > 1; j--) {
+		for (int i = 0; i < j; i++) {
+			a_b_temp += to_string(sum[((a_b[i] - '0') + (a_b[i + 1] - '0'))]);		//i와 i+1을 더한배열에 -'0'을 연산하여 int로 변환한후 배열에서 해당하는값을 찾아 넣는다
+
+		}
+		swap(a_b_temp, a_b);
+		a_b_temp = "";
+	}
+
+	cout << a_b << endl;
 
 
 	return 0;
