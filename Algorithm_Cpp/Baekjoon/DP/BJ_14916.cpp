1bcc0f099453f8ef38fb5846e2ac9ee13d2e9983
YakultLover <rianbowgift@gmail.com>
2022-03-23 00:00:21 +0900

    설탕배달(2839)와 비슷하지만 약간다른문제

---
+++ Algorithm_Cpp/Baekjoon/DP/BJ_14916.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+
+int dp[100006];
+int BJ_14916() {
+	int n;
+	cin >> n;
+
+	dp[1] = -1;
+	dp[2] = 1;
+	dp[3] = -1;
+	dp[4] = 2;
+	dp[5] = 1;
+	
+
+
+	for (int i = 6; i <= n;i++) {
+
+		if (dp[i-2]) {
+
+			dp[i] = dp[i-2] + 1;
+		}
+		if (dp[i - 5] && dp[i-5]!=-1) {
+			
+			dp[i] = dp[i - 5] + 1;
+			
+		}
+
+	}
+
+	if (n == 3 || n == 1) {
+		cout << "-1";
+	}else{
+		cout << dp[n];
+	}
+	
+	
+
+
+
+
+	return 0;
+}
