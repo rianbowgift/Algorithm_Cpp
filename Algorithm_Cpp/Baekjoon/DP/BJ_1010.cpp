135067ff9c35b6cb05627d6be9d45c2144bda01e
YakultLover <rianbowgift@gmail.com>
2022-03-22 12:30:06 +0900

    기본문제라는데 어려웠음.

---
+++ Algorithm_Cpp/Baekjoon/DP/BJ_1010.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int arr_1010[31][31];
+
+int dp_1010(int n, int m) {
+	if (n==m || n ==0) {
+		return 1;
+	}
+	if (arr_1010[n][m]) {
+		return arr_1010[n][m];
+	}
+	return arr_1010[n][m] = dp_1010(n , m-1) + dp_1010(n - 1, m - 1);
+
+}
+
+int main() {
+	int a;
+	cin >> a;
+
+
+	for (int i = 0; i < a; i++) {
+		int n, m;
+		cin >> n >> m;
+		cout << dp_1010(n, m) << endl;;
+	}
+
+
+
+
+	return 0;
+}
