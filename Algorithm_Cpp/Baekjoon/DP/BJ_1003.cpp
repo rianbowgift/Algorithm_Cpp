54cd94a85ea9dd05ac6cffd384804f407211754b
YakultLover <rianbowgift@gmail.com>
2022-03-25 05:41:28 +0900

    DP란 결국 점화식찾기가 아닐까

--- Algorithm_Cpp/Baekjoon/DP/BJ_1003.cpp
+++ Algorithm_Cpp/Baekjoon/DP/BJ_1003.cpp
@@ -1,31 +1,63 @@
 ﻿#include<iostream>
+#include<string>
 using namespace std;
 
-int fibonacci(int n) {
+int dp_1003[41];
+
+int fibonacci_1003(int n) {
     if (n == 0) {
-        printf("0");
         return 0;
     }
     else if (n == 1) {
-        printf("1");
+
         return 1;
     }
     else {
-        return fibonacci(n-1) + fibonacci(n-2);
+        if (dp_1003[n]) {
+
+            return dp_1003[n];
+        }
+        else {
+
+            return dp_1003[n] = fibonacci_1003(n - 1) + fibonacci_1003(n - 2);
+        }
+        
     }
 }
 
-int main() {
+int BJ_1003() {
 	int a;
 	cin >> a;
-
-
+    string* arr = new string[a];
+    
 	for (int i = 0; i < a; i++) {
         int n;
         cin >> n;
-        fibonacci(n);
+        if (n == 0) {
+            arr[i] = "1 0";
+        }
+
+        if ( n==1){
+            arr[i] = "0 1";
+        }
+
+        if (n == 2) {
+            arr[i] = "1 1";
+        }
+
+
+        if(n>=3) {
+            fibonacci_1003(n);
+            arr[i] = to_string(dp_1003[n - 1]) + " " + to_string(dp_1003[n]);
+            
+        }
+
 
 	}
+
+    for (int i = 0; i < a; i++) {
+        cout << arr[i] << endl;
+    }
 
 
 
