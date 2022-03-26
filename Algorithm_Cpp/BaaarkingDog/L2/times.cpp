7f564dfe92b710d4575d09db684759dae4459f70
YakultLover <rianbowgift@gmail.com>
2022-03-26 23:14:44 +0900

    바킹독 알고리즘으로 잠시선회(보류)

---
+++ Algorithm_Cpp/BaaarkingDog/L2/times.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int n = 100000;
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+
+		if (i % 3 == 0) {
+			sum += i;
+		}
+		else if (i % 5 == 0) {
+			sum += i;
+		}
+	}
+	cout << sum;
+
+	return 0;
+}
