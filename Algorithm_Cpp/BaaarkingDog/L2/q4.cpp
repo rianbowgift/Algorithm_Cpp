c17cb1c4696499b005501337047116f9b200a8dc
YakultLover <rianbowgift@gmail.com>
2022-03-27 17:44:53 +0900

    바킹독 스터디

--- Algorithm_Cpp/BaaarkingDog/L2/q4.cpp
+++ Algorithm_Cpp/BaaarkingDog/L2/q4.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+int q4() {
 
 	int n;
 	cin >> n;
---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_10808.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	string a;
+	cin >> a;
+	int arr[26];
+
+	for (int i = 0; i < 26; i++) {
+		arr[i] = 0;
+	}
+
+
+	for (int i = 0; i < a.size(); i++) {
+		
+		arr[a.at(i)-97] += 1; 
+		
+		
+	}
+	
+	for (int i = 0; i < 26; i++) {
+		cout << arr[i] << " ";
+	}
+	
+
+	return 0;
+
+
+}
