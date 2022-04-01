0c567fcf56177809e7980d1d58f2e52a4bb9b8cd
YakultLover <rianbowgift@gmail.com>
2022-04-02 06:23:14 +0900

    void retrun을 안하여 시간낭비. void도 return이 필요할땐 필요하다

--- Algorithm_Cpp/Baekjoon/Recursion/BJ_11729.cpp
+++ Algorithm_Cpp/Baekjoon/Recursion/BJ_11729.cpp
@@ -1,10 +1,29 @@
 #include<iostream>
 using namespace std;
 
-int main() {
-	int a;
+
+void recursion(int a, int b, int n) {
+	if (n == 1) {
+		cout << a << ' ' << b << '\n';
+		return;
+	}
+	recursion(a, 6 - a - b, n - 1);
+	cout << a << ' ' << b << '\n';
 	
 
+	recursion(6 - a - b, b, n - 1);
+	
+}
 
 
+int main() {
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int a;
+	cin >> a;
+	cout << (1 << a) - 1 << '\n';
+	recursion(1,3,a);
+
+	
 }
