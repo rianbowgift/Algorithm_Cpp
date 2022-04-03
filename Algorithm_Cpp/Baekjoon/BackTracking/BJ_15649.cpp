34ddd3903e0855cb58c15d3d43e908afb54d9c9d
YakultLover <rianbowgift@gmail.com>
2022-04-03 15:10:31 +0900

    전역선언을위해 전체주석

--- Algorithm_Cpp/Baekjoon/BackTracking/BJ_15649.cpp
+++ Algorithm_Cpp/Baekjoon/BackTracking/BJ_15649.cpp
@@ -1,10 +1,38 @@
 #include<iostream>
 using namespace std;
 
+int n,m;
+int arr[10];
+int isused[10];
+
+void backtrack(int k) {
+	if (m ==k) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i < n; i++) {
+		if (!isused[i]) {
+			arr[k] = i;
+			isused[i] = 1;
+			backtrack(k + 1);
+			isused[i] = 0;
+
+		}
+	}
+	
+
+
+}
+
 int main() {
 
-
-
+	
+	cin >> n >> m;
+	backtrack(0);
 
 
 	return 0;
