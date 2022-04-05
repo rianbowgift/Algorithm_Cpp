51ebfa25b69de18a1143137041034cafc1204e7a
YakultLover <rianbowgift@gmail.com>
2022-04-06 04:36:09 +0900

    한번 실패했던 문제. 빠진부분이 없는듯한데 틀리다.

--- Algorithm_Cpp/Baekjoon/BackTracking/BJ_9663.cpp
+++ Algorithm_Cpp/Baekjoon/BackTracking/BJ_9663.cpp
@@ -1,54 +1,54 @@
-#include<iostream>
-using namespace std;
- 
-int n;
-int arr[100][100];
-bool used[100][100];
-
-int sum = 0;
-
-void back(int a) {
-	
-	if (a == n) {
-		sum++;
-		return;
-	}
-
-	for (int i = 1; i <= n; i++) {		
-		for (int j = 1; j <= n; j++) {
-			if (!used[i][j]) {
-				
-				
-				for (int k = 1; k <= n; k++) {
-					if (used[k][j]) {
-						
-					}
-				}
-
-
-
-
-			}
-		}
-	}
-
-	
-	
-
-}
-
-
-
-
-
-int main() {
-	cin >> n;
-
-	for (int i = 0; i < n; i++) {
-		arr[i][1] = 1;
-		used[i][1] = true;
-	}
-
-	back(0);
-
-}
+//#include<iostream>
+//using namespace std;
+// 
+//int n;
+//int arr[100][100];
+//bool used[100][100];
+//
+//int sum = 0;
+//
+//void back(int a) {
+//	
+//	if (a == n) {
+//		sum++;
+//		return;
+//	}
+//
+//	for (int i = 1; i <= n; i++) {		
+//		for (int j = 1; j <= n; j++) {
+//			if (!used[i][j]) {
+//				
+//				
+//				for (int k = 1; k <= n; k++) {
+//					if (used[k][j]) {
+//						
+//					}
+//				}
+//
+//
+//
+//
+//			}
+//		}
+//	}
+//
+//	
+//	
+//
+//}
+//
+//
+//
+//
+//
+//int main() {
+//	cin >> n;
+//
+//	for (int i = 0; i < n; i++) {
+//		arr[i][1] = 1;
+//		used[i][1] = true;
+//	}
+//
+//	back(0);
+//
+//}
---
+++ Algorithm_Cpp/Baekjoon/Failure/BJ_1673.cpp
@@ -0,0 +1,34 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//
+//
+//
+//	int a, b;
+//	int sum = 0;
+//	int cu = 0;
+//	while (cin >> a >> b) {
+//
+//
+//
+//
+//		sum += a;
+//		cu += a;
+//		
+//		while (cu >= b) {
+//			int temp = cu % b;
+//			cu = cu / b;
+//			sum += cu;
+//			cu = cu + temp;
+//		}
+//
+//		cout << sum;
+//
+//		a, b, sum=0, cu = 0;
+//
+//	}
+//	
+//
+//
+//}
+//
