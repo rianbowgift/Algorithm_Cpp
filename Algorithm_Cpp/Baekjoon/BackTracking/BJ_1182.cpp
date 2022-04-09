a05f5800d06c4dd24c987b38eebee11ff5887852
YakultLover <rianbowgift@gmail.com>
2022-04-09 10:20:35 +0900

    귀납/백트렉킹 강의완독후 다시볼것.

--- Algorithm_Cpp/Baekjoon/BackTracking/BJ_1182.cpp
+++ Algorithm_Cpp/Baekjoon/BackTracking/BJ_1182.cpp
@@ -1,54 +1,54 @@
-#include<iostream>
-using namespace std;
-
-
-int a, b;
-int* arr = new int[a+1];
-int sum;
-
-void back(int count) {
-	if (count > a) {
-		return;
-	}
-
-	if (count == 3) {
-		cout << "hi";
-	}
-		int temp=0;
-
-		for (int i = count; i <= a; i++) {
-
-			temp += arr[i];
-			if (temp == b) {
-				sum++;
-				
-			}
-		}
-
-
-		back(count+1);
-
-
-
-
-	
-
-}
-
-
-
-int main() {
-
-
-	cin >> a >> b;
-	
-	for (int i = 0; i < a; i++) {
-		cin >> arr[i];
-	}
-	
-	
-
-	back(0);
-
-	cout << sum;
-}
+//#include<iostream>
+//using namespace std;
+//
+//
+//int a, b;
+//int* arr = new int[a+1];
+//int sum;
+//
+//void back(int count) {
+//	if (count > a) {
+//		return;
+//	}
+//
+//	if (count == 3) {
+//		cout << "hi";
+//	}
+//		int temp=0;
+//
+//		for (int i = count; i <= a; i++) {
+//
+//			temp += arr[i];
+//			if (temp == b) {
+//				sum++;
+//				
+//			}
+//		}
+//
+//
+//		back(count+1);
+//
+//
+//
+//
+//	
+//
+//}
+//
+//
+//
+//int main() {
+//
+//
+//	cin >> a >> b;
+//	
+//	for (int i = 0; i < a; i++) {
+//		cin >> arr[i];
+//	}
+//	
+//	
+//
+//	back(0);
+//
+//	cout << sum;
+//}
