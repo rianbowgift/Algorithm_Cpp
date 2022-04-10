865ff1d70d3e3657453b7b4fd44937775a6bba81
YakultLover <rianbowgift@gmail.com>
2022-04-10 13:12:20 +0900

    점화식을 오래생각했지만 점화식에서 답을 도출하기가 쉬웠음

---
+++ Algorithm_Cpp/Baekjoon/DP/BJ_9095.cpp
@@ -0,0 +1,35 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//int sum[13];
+//
+//
+//int main() {
+//
+//	int a, n;
+//
+//	cin >> a;
+//
+//	sum[1] = 1;
+//	sum[2] = 2;
+//	sum[3] = 4;
+//
+//
+//	for (int i = 4; i <= 10; i++) {
+//		sum[i] = sum[i - 3] + sum[i - 2] + sum[i - 1];
+//	}
+//
+//
+//
+//	for (int i = 0; i < a; i++) {
+//
+//		cin >> n;
+//		cout << sum[n] << "\n";
+//	}
+//	
+//
+//
+//
+//
+//}
