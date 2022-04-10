c56f34c238cc0ac813821ad525215ea11f4c46e4
YakultLover <rianbowgift@gmail.com>
2022-04-11 08:14:31 +0900

    구간합. 잘생각해보면 DP.

--- Algorithm_Cpp/Baekjoon/DP/BJ_11659.cpp
+++ Algorithm_Cpp/Baekjoon/DP/BJ_11659.cpp
@@ -1,9 +1,35 @@
 #include<iostream>
 using namespace std;
 
+
+int sum[100002];
+
+
 int main() {
 
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
+	
+	int a, b;
+
+
+	cin >> a >> b;
+
+	for (int i = 1; i <= a; i++) {
+		cin >> sum[i];
+		sum[i] += sum[i - 1];
+	}
+	
+
+	for (int i = 0; i < b; i++) {
+		int x, y;
+		cin >> x >> y;
+
+		cout << sum[y] - sum[x - 1] << '\n';
+
+	}
+	
 
 
 }
