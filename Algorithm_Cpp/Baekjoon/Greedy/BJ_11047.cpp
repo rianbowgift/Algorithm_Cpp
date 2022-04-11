ff4dd78b24d43f35c11cb566578f847a2767784f
YakultLover <rianbowgift@gmail.com>
2022-04-11 09:03:49 +0900

    동전0. 그리디.

---
+++ Algorithm_Cpp/Baekjoon/Greedy/BJ_11047.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int a, b;
+	cin >> a >> b;
+
+	int* arr = new int[a];
+	int sum = 0;
+	int count = 0;
+	int i = a - 1;
+
+	for (int i = 0; i < a; i++) {
+		cin >> arr[i];
+	}
+
+	
+
+	
+	
+	while (true) {
+		if (b == sum) {
+			cout << count;
+			break;
+		}
+
+		if (arr[i] > b-sum) {
+			i--;
+			continue;
+		}
+		
+		sum += arr[i];
+		count++;
+
+
+
+	}
+
+
+
+	
+}
