38ec84ad267fc749cc944cb047485fb5cac3c56e
YakultLover <rianbowgift@gmail.com>
2022-04-16 07:26:42 +0900

    STL정렬소팅

---
+++ Algorithm_Cpp/Baekjoon/Sort/BJ_1026.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int cmp(int a, int b) {
+	if (a > b) {
+		return b;
+	}
+	else {
+		return a;
+	}
+}
+
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a;
+	int sum = 0;
+	cin >> a;
+	int* arr1 = new int[a];
+	int* arr2 = new int[a];
+
+	
+
+	for (int i = 0; i < a; i++) {
+		cin >> arr1[i];
+	}
+	for (int i = 0; i < a; i++) {
+		cin >> arr2[i];
+	}
+
+	sort(arr1, arr1 + a);
+	sort(arr2, arr2 + a,cmp);
+
+	for (int i = 0; i < a; i++) {
+		sum += arr1[i] * arr2[i];
+	}
+
+	cout << sum;
+
+
+	
+
+	
+
+}
--- Algorithm_Cpp/Baekjoon/Sort/BJ_11652.cpp
+++ Algorithm_Cpp/Baekjoon/Sort/BJ_11652.cpp
@@ -1,54 +1,54 @@
-#include<iostream>
-#include<algorithm>
-
-using namespace std;
-
-
-long long arr[1000000];
-
-int main() {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	long long num = 10000000;	//mxval
-	int count = 0;	//maxcnt
-	int temp_count = 0;	//cnt
-
-
-
-
-	int a;
-	cin >> a;
-
-	for (int i = 0; i < a; i++) {
-		cin >> arr[i];
-	}
-
-	sort(arr,arr+a);
-
-	for (int i = 0; i < a; i++) {
-		if (i == 0 || arr[i-1] == arr[i]) {
-			temp_count++;
-
-		}
-		else {
-			if (temp_count > count) {
-				count = temp_count;
-				num = arr[i-1];
-				
-			}
-			temp_count = 1;
-		}
-
-	}
-
-	if (temp_count > count) {
-		num = arr[a - 1];
-	}
-	cout << num;
-
-
-
-
-}
+//#include<iostream>
+//#include<algorithm>
+//
+//using namespace std;
+//
+//
+//long long arr[1000000];
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	long long num = 10000000;	//mxval
+//	int count = 0;	//maxcnt
+//	int temp_count = 0;	//cnt
+//
+//
+//
+//
+//	int a;
+//	cin >> a;
+//
+//	for (int i = 0; i < a; i++) {
+//		cin >> arr[i];
+//	}
+//
+//	sort(arr,arr+a);
+//
+//	for (int i = 0; i < a; i++) {
+//		if (i == 0 || arr[i-1] == arr[i]) {
+//			temp_count++;
+//
+//		}
+//		else {
+//			if (temp_count > count) {
+//				count = temp_count;
+//				num = arr[i-1];
+//				
+//			}
+//			temp_count = 1;
+//		}
+//
+//	}
+//
+//	if (temp_count > count) {
+//		num = arr[a - 1];
+//	}
+//	cout << num;
+//
+//
+//
+//
+//}
