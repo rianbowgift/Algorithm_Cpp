5873af20ba48b23f7baee8c4371e54b6b59dde31
YakultLover <rianbowgift@gmail.com>
2022-04-17 13:03:30 +0900

    해쉬맵 정렬. string temp 상태에서 arr1[temp] 식으로 비교할수 있다는점을 배움

--- Algorithm_Cpp/Baekjoon/Sort/BJ_1764.cpp
+++ Algorithm_Cpp/Baekjoon/Sort/BJ_1764.cpp
@@ -1,1 +1,41 @@
-#include
+//#include<iostream>
+//#include<algorithm>
+//#include<vector>
+//#include<map>
+//
+//using namespace std;
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//
+//	map<string, int> arr1;
+//	vector<string> arr2;
+//	int a, b;
+//
+//	cin >> a >> b;
+//
+//
+//	for (int i = 0; i < a+b; i++) {
+//		string temp;
+//		cin >> temp;
+//		if (arr1[temp]) {
+//			arr2.push_back(temp);
+//		}
+//		else {
+//			arr1[temp] = 1;
+//		}
+//	
+//	}
+//
+//
+//	sort(arr2.begin(), arr2.end());
+//	cout << arr2.size() << "\n";
+//	for (int i = 0; i < arr2.size(); i++) {
+//	
+//		cout << arr2[i] << "\n";
+//	}
+//
+//
+//}
