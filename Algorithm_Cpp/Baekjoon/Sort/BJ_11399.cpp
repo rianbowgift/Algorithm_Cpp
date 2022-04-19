f884b55eee7a7302d96ebc9e23f4106eebb42372
YakultLover <rianbowgift@gmail.com>
2022-04-19 13:36:30 +0900

    DP라고 생각했는데, 의외로 n값이 작아서 간단한 그리디로 해결

---
+++ Algorithm_Cpp/Baekjoon/Sort/BJ_11399.cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//
+//int arr[1002];
+//
+//int main() {
+//	
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	int sum = 0;
+//
+//	int a;
+//	cin >> a;
+//	
+//	for (int i = 0; i < a; i++) {
+//		cin >> arr[i];
+//	}
+//
+//
+//	sort(arr, arr + a);
+//
+//	for (int i = 0; i < a; i++) {
+//		for (int j = 0; j <= i; j++) {
+//			sum += arr[j];
+//		}
+//	}
+//
+//	cout << sum;
+//
+//}
