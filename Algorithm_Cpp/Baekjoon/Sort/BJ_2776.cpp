0882e3e5cb7bfdfcb3252ea29a8133f7e4b159a4
YakultLover <rianbowgift@gmail.com>
2022-04-20 15:25:34 +0900

    STL을 사용하여 간단히 해결

--- Algorithm_Cpp/Baekjoon/Sort/BJ_2776.cpp
+++ Algorithm_Cpp/Baekjoon/Sort/BJ_2776.cpp
@@ -1,58 +1,58 @@
-#include<iostream>
-#include<map>
-using namespace std;
-
-int main() {
-	
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-
-
-	int n;
-	cin >> n;
-	int a, b;
-
-
-	
-	for (int i = 0; i < n; i++) {
-		cin >> a;
-		map<int, int> arr1;
-
-		for (int i = 0; i < a; i++) {
-			int temp;
-			cin >> temp;
-			arr1.insert({ temp,1 });
-
-		}
-
-		cin >> b;
-		int* arr2 = new int[b];
-
-
-		for (int i = 0; i < b; i++) {
-			int temp;
-			cin >> temp;
-
-			//cout << "이거임 -> " << arr1[temp] << " \n";
-
-			if (arr1[temp]) {
-				arr2[i] = 1;
-			}
-			else {
-				arr2[i] = 0;
-			}
-		}
-
-		for (int i = 0; i < b; i++) {
-			cout << arr2[i] << "\n";
-		}
-
-		
-	}
-
-
-
-
-
-}
+//#include<iostream>
+//#include<map>
+//using namespace std;
+//
+//int main() {
+//	
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//
+//
+//	int n;
+//	cin >> n;
+//	int a, b;
+//
+//
+//	
+//	for (int i = 0; i < n; i++) {
+//		cin >> a;
+//		map<int, int> arr1;
+//
+//		for (int i = 0; i < a; i++) {
+//			int temp;
+//			cin >> temp;
+//			arr1.insert({ temp,1 });
+//
+//		}
+//
+//		cin >> b;
+//		int* arr2 = new int[b];
+//
+//
+//		for (int i = 0; i < b; i++) {
+//			int temp;
+//			cin >> temp;
+//
+//			//cout << "이거임 -> " << arr1[temp] << " \n";
+//
+//			if (arr1[temp]) {
+//				arr2[i] = 1;
+//			}
+//			else {
+//				arr2[i] = 0;
+//			}
+//		}
+//
+//		for (int i = 0; i < b; i++) {
+//			cout << arr2[i] << "\n";
+//		}
+//
+//		
+//	}
+//
+//
+//
+//
+//
+//}
