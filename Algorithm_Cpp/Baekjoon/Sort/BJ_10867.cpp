6ca78e32bc395b11ae6f53fc9c1608beaeb9d46d
YakultLover <rianbowgift@gmail.com>
2022-04-21 07:34:42 +0900

    STL 연습 소팅

---
+++ Algorithm_Cpp/Baekjoon/Sort/BJ_10867.cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//
+//int main() {
+//
+//	
+//	int a;
+//	vector<int> arr;
+//	cin >> a;
+//
+//	for (int i = 0; i < a; i++) {
+//		int temp;
+//		cin >> temp;
+//		arr.push_back(temp);
+//	}
+//
+//	sort(arr.begin(),arr.end());	//정렬
+//
+//	arr.erase(unique(arr.begin(), arr.end()),arr.end());	//중복값은 가장뒤로 날리고 삭제
+//
+//	for (int i = 0; i < arr.size(); i++) {
+//		cout << arr[i] << " ";
+//	}
+//
+//	
+//	
+//
+//
+//}
