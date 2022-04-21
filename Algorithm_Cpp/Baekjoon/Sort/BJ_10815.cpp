77c38aaeb99ce6c42b6b713da15de960e4a8d7a9
YakultLover <rianbowgift@gmail.com>
2022-04-22 08:24:05 +0900

    STL을 이용한 소팅. 자주하다보니 꽤 할만해짐

---
+++ Algorithm_Cpp/Baekjoon/Sort/BJ_10815.cpp
@@ -0,0 +1,34 @@
+//#include<iostream>
+//#include<map>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int a, b;
+//	cin >> a;
+//	map<int, int> arr;
+//	for (int i = 0; i < a; i++) {
+//		int temp;
+//		cin >> temp;
+//		arr[temp]++;
+//
+//	}
+//	
+//	cin >> b;
+//	for (int i = 0; i < b; i++) {
+//		int temp;
+//		cin >> temp;
+//
+//		if (arr[temp]) {
+//			cout << "1" << " ";
+//		}
+//		else {
+//			cout << "0" << " ";
+//		}
+//	}
+//
+//
+//}
