d29a9dce917bb6f672356cd651b1a5fa0cde5dd4
YakultLover <rianbowgift@gmail.com>
2022-04-22 17:31:37 +0900

    문제가 잘못됨. 시간 너무많이줌. 아무런 알고리즘 없이 단순소팅으로 구현됨.

---
+++ Algorithm_Cpp/Baekjoon/Sort/BJ_2075.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//
+//bool reverse_num(int a, int b) {
+//	
+//	return a > b;
+//}
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int n;
+//
+//	cin >> n;
+//	int* arr = new int[n * n];
+//	fill(arr, arr+ (n*n), 0);
+//	
+//	for (int i = 0; i < n * n; i++) {
+//		cin >> arr[i];
+//	}
+//	
+//
+//	sort(arr, arr + (n * n), reverse_num);
+//
+//	cout << arr[n-1];
+//	
+//
+//
+//
+//
+//
+//
+//
+//}
