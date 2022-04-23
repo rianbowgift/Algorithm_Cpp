f66c4ea2a161b7ef14961c0b9f06a1d32a41d596
YakultLover <rianbowgift@gmail.com>
2022-04-24 07:26:12 +0900

    이터레이터, STL-List 학습

--- Algorithm_Cpp/Baekjoon/LinkedList/BJ_1406.cpp
+++ Algorithm_Cpp/Baekjoon/LinkedList/BJ_1406.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
+#include<algorithm>
+#include<list>
 using namespace std;
+
 
 int main() {
 
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-
-
+	
+	list<int> L = { 1,2 };
+	list<int>::iterator t = L.begin(); // L
+	
 
 
 }
