08e0d019d0905da39be864e97f7261d6385cc815
YakultLover <rianbowgift@gmail.com>
2022-04-25 00:01:10 +0900

    에디터보다 조금더 어려운 리스트

---
+++ Algorithm_Cpp/Baekjoon/LinkedList/BJ_5397.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<list>
+#include<string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a;
+	cin >> a;
+
+	for (int z = 0; z < a; z++) {
+
+		string temp;
+		cin >> temp;
+		list<char> keylog_list;
+		for (int i = 0; i < temp.size(); i++) {
+			keylog_list.push_back(temp[i]);
+		}
+
+		
+
+		
+
+
+
+
+
+
+
+	}
+
+
+
+	
+}
