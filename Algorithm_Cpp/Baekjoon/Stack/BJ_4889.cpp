6026ea4c7efdb6462ac1d338d0ba3cc8615a4460
YakultLover <rianbowgift@gmail.com>
2022-04-26 00:11:36 +0900

    스택-괄호문제 트라이

---
+++ Algorithm_Cpp/Baekjoon/Stack/BJ_4889.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+
+	while (true) {
+		string temp;
+		
+		cin >> temp;
+		if (temp[0] == '-') {
+			break;
+		}
+
+		
+
+
+
+	}
+
+
+
+
+}
