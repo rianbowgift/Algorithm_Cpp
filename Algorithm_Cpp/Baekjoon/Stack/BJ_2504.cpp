85d7e9a18cde7995fd149bcd2606a6e5be0c161d
YakultLover <rianbowgift@gmail.com>
2022-05-05 01:15:26 +0900

    의외로 생각할점이 많음. 괄호문제

--- Algorithm_Cpp/Baekjoon/Stack/BJ_2504.cpp
+++ Algorithm_Cpp/Baekjoon/Stack/BJ_2504.cpp
@@ -1,11 +1,98 @@
-#include<iostream>
-#include<stack>
-using namespace std;
-
-int main() {
-
-	
-
-
-
-}
+//#include<iostream>
+//#include<stack>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	string a;
+//	cin >> a;
+//	stack<int> st;
+//	int sum = 0;
+//
+//	for (int i = 0; i < a.size(); i++) {
+//		// [ -9    ] -7		( -60    ) -59
+//		//int 형으로 통일하여 사용
+//		st.push(a[i]-100);
+//		
+//		if (st.top() == -59) {
+//			st.pop();
+//			int save = 0;
+//			while (true) {
+//				if (st.empty()) {
+//					cout << "0";
+//					return 0;
+//				}
+//				if (st.top() == -7 || st.top() == -9) {
+//					cout << "0";
+//					return 0;
+//				}
+//				
+//				if (st.top() == -60) {
+//					st.pop();
+//					if (save != 0) {
+//						st.push(save * 2);
+//					}
+//					else {
+//						st.push(2);
+//					}
+//					break;
+//					
+//				}
+//				else {
+//					save += st.top();
+//					st.pop();
+//				}
+//			}
+//		}
+//		else if (st.top() == -7) {
+//			st.pop();
+//			int save = 0;
+//			while (true) {
+//				if (st.empty()) {
+//					cout << "0";
+//					return 0;
+//				}
+//				if (st.top() == -60 || st.top() == -59) {
+//					cout << "0";
+//					return 0;
+//				}
+//				
+//				if (st.top() == -9) {
+//					st.pop();
+//					if (save != 0) {
+//						st.push(save * 3);
+//					}
+//					else {
+//						st.push(3);
+//					}
+//					break;
+//
+//				}
+//				else {
+//					save += st.top();
+//					st.pop();
+//				}
+//			}
+//		}
+//	
+//
+//	}
+//
+//
+//	while (!st.empty()) {
+//		if (st.top() == -9 || st.top() == -7 || st.top() == -59 || st.top() == -60) {
+//			cout << "0";
+//			return 0;
+//		}
+//
+//		sum += st.top();
+//		st.pop();
+//
+//	}
+//	cout << sum;
+//
+//
+//}
