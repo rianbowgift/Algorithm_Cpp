3dc892b2d651d4ea298b92c5143d2a14063a5237
YakultLover <rianbowgift@gmail.com>
2022-05-12 00:28:53 +0900

    재귀,백트랙킹 연습

--- Algorithm_Cpp/BaaarkingDog/ETC/recursion_test.cpp
+++ Algorithm_Cpp/BaaarkingDog/ETC/recursion_test.cpp
@@ -1,10 +1,28 @@
 #include<iostream>
 using namespace std;
+
+int c;
+int re(int a, int b) {
+	if (b == 1) {
+		return a;
+	}
+	
+	return (a * (re(a,b-1) % c)%c);
+	
+	
+
+}
+
 int main() {
 
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
+	int a, b;
+
+	cin >> a >> b >> c;
+
+	cout << re(a, b);
 
 
 }
