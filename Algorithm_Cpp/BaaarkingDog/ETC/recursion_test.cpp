940f5bfc4283385df9cd1cde39d4e9dc9a4274b0
YakultLover <rianbowgift@gmail.com>
2022-05-13 11:34:48 +0900

    재귀 실패문제 다시

--- Algorithm_Cpp/BaaarkingDog/ETC/recursion_test.cpp
+++ Algorithm_Cpp/BaaarkingDog/ETC/recursion_test.cpp
@@ -1,28 +1,31 @@
-#include<iostream>
-using namespace std;
-
-int c;
-int re(int a, int b) {
-	if (b == 1) {
-		return a;
-	}
-	
-	return (a * (re(a,b-1) % c)%c);
-	
-	
-
-}
-
-int main() {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	int a, b;
-
-	cin >> a >> b >> c;
-
-	cout << re(a, b);
-
-
-}
+//#include<iostream>
+//using namespace std;
+//
+//
+//int re(int a) {
+//
+//
+//	
+//
+//	
+//
+//	
+//	
+//
+//}
+//
+//
+//int main() {
+//
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int a;
+//
+//	cin >> a;
+//
+//	cout << re(a); 
+//
+//
+//}
---
+++ Algorithm_Cpp/Baekjoon/Recursion/BJ_18511.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+
+void reculsion(int a,int* b,int sum) {
+	
+	
+
+	
+
+}
+
+
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a;
+
+
+
+	
+
+}
