626c5de8b547c07efbe5f1aa4502147c4b8e2b6d
YakultLover <rianbowgift@gmail.com>
2022-05-13 18:35:50 +0900

    재귀 연습

--- Algorithm_Cpp/Baekjoon/Recursion/BJ_7113.cpp
+++ Algorithm_Cpp/Baekjoon/Recursion/BJ_7113.cpp
@@ -1,40 +1,40 @@
-#include<iostream>
-using namespace std;
-
-
-int sum;
-
-
-void re(int a, int b) {
-	if (a == b) {
-		sum++;
-		return;
-	}
-
-	if (a > b) {
-		sum++;
-		re(a - b, b);
-	}
-	else {
-		sum++;
-		re(b - a, a);
-	}
-
-
-}
-
-int main() {
-
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-
-	int a, b;
-	cin >> a >> b;
-
-	re(a, b);
-	cout << sum;
-
-
-}
+//#include<iostream>
+//using namespace std;
+//
+//
+//int sum;
+//
+//
+//void re(int a, int b) {
+//	if (a == b) {
+//		sum++;
+//		return;
+//	}
+//
+//	if (a > b) {
+//		sum++;
+//		re(a - b, b);
+//	}
+//	else {
+//		sum++;
+//		re(b - a, a);
+//	}
+//
+//
+//}
+//
+//int main() {
+//
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//
+//	int a, b;
+//	cin >> a >> b;
+//
+//	re(a, b);
+//	cout << sum;
+//
+//
+//}
