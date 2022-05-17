8c957f4f66772d9c43a1064a421997b34e108b60
YakultLover <rianbowgift@gmail.com>
2022-05-17 11:46:54 +0900

    접근방식 변경 스트링->배열

---
+++ Algorithm_Cpp/Baekjoon/Recursion/BJ_16505.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int arr[1300][1300];
+
+string re(int a) {
+	
+
+	
+
+
+}
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int a;
+	cin >> a;
+
+
+	re(a);
+
+
+
+}
