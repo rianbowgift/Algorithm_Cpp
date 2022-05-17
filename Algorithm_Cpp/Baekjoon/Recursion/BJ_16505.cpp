27326dcfd06bc1e16a53bb45b54aa6dfcbb45311
YakultLover <rianbowgift@gmail.com>
2022-05-17 20:37:50 +0900

    재귀별찍기 첫성공

--- Algorithm_Cpp/Baekjoon/Recursion/BJ_16505.cpp
+++ Algorithm_Cpp/Baekjoon/Recursion/BJ_16505.cpp
@@ -1,27 +1,50 @@
-#include<iostream>
-
-using namespace std;
-
-int arr[1300][1300];
-
-string re(int a) {
-	
-
-	
-
-
-}
-
-int main() {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int a;
-	cin >> a;
-
-
-	re(a);
-
-
-
-}
+//#include<iostream>
+//#include<algorithm>
+//#include<cmath>
+//using namespace std;
+//
+//int arr[1026][1026];
+//
+//void re(int a,int x, int y) {
+//	if (a == 0) {
+//		arr[x][y] = 1;
+//		return;
+//	}
+//
+//	re(a - 1, x, y);
+//	re(a - 1, x, (pow(2, a - 1)) + y);
+//	re(a - 1, (pow(2, a - 1)) + x, y);
+//
+//
+//}
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	int a;
+//	cin >> a;
+//
+//
+//
+//	re(a,1,1);
+//	int sum = 0;
+//	for (int i = 1; i <= pow(2, a); i++) {
+//		for (int j = 1; j <= pow(2, a)-sum; j++) {
+//			if (arr[i][j] == 0) {
+//				cout << " ";
+//			}
+//			else {
+//				cout << "*";
+//			}
+//		}
+//		if (i == pow(2, a)) {
+//			break;
+//		}
+//		cout << "\n";
+//		sum++;
+//	}
+//
+//
+//
+//}
