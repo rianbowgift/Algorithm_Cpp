fe78797d7cf6a11c12e22c7fe16b9c0f1f3e6652
YakultLover <rianbowgift@gmail.com>
2022-05-23 22:43:28 +0900

    간단한 연습

---
+++ Algorithm_Cpp/Baekjoon/BackTracking/BJ_15651.cpp
@@ -0,0 +1,38 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//int n, m;
+//int arr[100];
+//bool isused[10];
+//
+//
+//void back(int a) {
+//	if (a == m) {
+//		for (int i = 0; i < m; i++) {
+//			cout << arr[i] << " ";
+//		}
+//		cout << "\n";
+//		return;
+//	}
+//
+//
+//	for (int i = 1; i <= n; i++) {
+//			arr[a] = i;
+//			back(a + 1);
+//	
+//
+//	}
+//
+//
+//
+//}
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cin >> n >> m;
+//
+//	back(0);
+//
+//}
