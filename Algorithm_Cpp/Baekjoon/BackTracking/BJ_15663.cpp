8f068b158a09178a5d83756d022760954eea590e
YakultLover <rianbowgift@gmail.com>
2022-05-25 04:29:47 +0900

    중복된 값 처리

---
+++ Algorithm_Cpp/Baekjoon/BackTracking/BJ_15663.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int n, m;
+int arr[100];
+bool isused[10];
+int nums[100];
+
+void re(int a,int back) {
+	if (m == a) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		
+		if (!isused[i] && back < nums[i]) {
+
+			arr[a] = nums[i];
+			isused[i] = 1;
+			re(a + 1, nums[i]);
+			isused[i] = 0;
+
+
+		}
+
+	}
+
+
+
+}
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> nums[i];
+	}
+
+	sort(nums + 1, nums + n + 1);
+
+	re(0,0);
+
+
+}
