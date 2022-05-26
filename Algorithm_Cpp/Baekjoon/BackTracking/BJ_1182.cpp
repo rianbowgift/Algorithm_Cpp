08afc45f0c60b01f65c3b1d0c0ce34cea18b6d87
YakultLover <rianbowgift@gmail.com>
2022-05-26 16:10:56 +0900

    쉽다고 생각하였으나 실패.

--- Algorithm_Cpp/Baekjoon/BackTracking/BJ_1182.cpp
+++ Algorithm_Cpp/Baekjoon/BackTracking/BJ_1182.cpp
@@ -1,54 +1,71 @@
-//#include<iostream>
-//using namespace std;
-//
-//
-//int a, b;
-//int* arr = new int[a+1];
-//int sum;
-//
-//void back(int count) {
-//	if (count > a) {
-//		return;
-//	}
-//
-//	if (count == 3) {
-//		cout << "hi";
-//	}
-//		int temp=0;
-//
-//		for (int i = count; i <= a; i++) {
-//
-//			temp += arr[i];
-//			if (temp == b) {
-//				sum++;
-//				
-//			}
-//		}
-//
-//
-//		back(count+1);
-//
-//
-//
-//
-//	
-//
-//}
-//
-//
-//
-//int main() {
-//
-//
-//	cin >> a >> b;
-//	
-//	for (int i = 0; i < a; i++) {
-//		cin >> arr[i];
-//	}
-//	
-//	
-//
-//	back(0);
-//
-//	cout << sum;
-//}
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int n, s;
+//int arr[1000];
+vector<int> arr2;
+bool isused[100];
+int nums[100];
+int sum;
+
+void re(int k) {
+	if (k == n) {
+		int temp = 0;
+		int size = arr2.size();
+		for (int i = 0; i < size; i++) {
+			temp += arr2.back();
+			arr2.pop_back();
+		}
+		if (s == temp) {
+			sum++;
+		}
+		return;
+	}
+	
+
+
+	for (int i = k; i < n; i++) {
+		
+		if (!isused[i]) {
+			
+			arr2.push_back(nums[i]);
+		
+			isused[i] = 1;
+			re(k+1);
+			isused[i] = 0;
+		}
+
+	}
+	
+
+}
+
+
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> s;
+	for (int i = 0; i < n; i++) {
+		cin >> nums[i];
+	}
+
+
+
+
+	for (int i = 0; i < n; i++) {
+		re(0);
+	}
+
+
+	cout << sum;
+	
+
+
+
+	
+}
