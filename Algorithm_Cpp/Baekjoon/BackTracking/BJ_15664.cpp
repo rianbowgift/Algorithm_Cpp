5cae80574700058cd4cfe24ae74c813a2c08a462
YakultLover <rianbowgift@gmail.com>
2022-05-28 15:11:46 +0900

    백트랙킹 nm

---
+++ Algorithm_Cpp/Baekjoon/BackTracking/BJ_15664.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int n, m;
+int arr[1000];
+bool isused[100];
+
+
+void re(int a) {
+
+
+
+
+}
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	
+
+
+	re(0);
+
+
+
+
+}
