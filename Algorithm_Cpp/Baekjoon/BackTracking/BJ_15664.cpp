8c951835cdbb1efd7950b1770245a356e6a9d2a3
YakultLover <rianbowgift@gmail.com>
2022-05-29 12:44:24 +0900

    중복검사 다시공부할것

--- Algorithm_Cpp/Baekjoon/BackTracking/BJ_15664.cpp
+++ Algorithm_Cpp/Baekjoon/BackTracking/BJ_15664.cpp
@@ -1,30 +1,42 @@
-#include<iostream>
-using namespace std;
-
-int n, m;
-int arr[1000];
-bool isused[100];
-
-
-void re(int a) {
-
-
-
-
-}
-
-int main() {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	cin >> n >> m;
-	
-
-
-	re(0);
-
-
-
-
-}
+//#include<iostream>
+//using namespace std;
+//
+//int n, m;
+//int arr[1000];
+//bool isused[100];
+//int nums[10];
+//
+//
+//void re(int a) {
+//	if (a == m) {
+//		for (int i = 0; i < m; i++) {
+//			cout << arr[i] << ' ';
+//		}
+//		cout << "\n";
+//		return;
+//	}
+//
+//
+//
+//
+//
+//}
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	cin >> n >> m;
+//	
+//	for (int i = 1; i <= n; i++) {
+//		cin >> nums[i];
+//	}
+//
+//
+//	re(0);
+//
+//
+//
+//
+//}
