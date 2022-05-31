0c268f7a2803a118a033073c6c33ddeee5d6160e
YakultLover <rianbowgift@gmail.com>
2022-06-01 01:31:17 +0900

    감잡히는 백트렉킹

--- Algorithm_Cpp/Baekjoon/BackTracking/BJ_10819.cpp
+++ Algorithm_Cpp/Baekjoon/BackTracking/BJ_10819.cpp
@@ -1,56 +1,62 @@
 //#include<iostream>
-//#include<algorithm>
-//#include<vector>
-//#include<string>
-//
 //using namespace std;
 //
-//int n, m;
-//int arr[100];
+//int n;
+//int arr[10];
 //bool isused[10];
-//int nums[100];
+//int sum;
+//int nums[10];
 //
 //
 //void re(int a) {
-//	if (a == m) {
-//		for (int i = 0; i < m; i++) {
-//			cout << arr[i] << ' ';
+//	if (a == n) {
+//		int temp = 0;
+//		for (int i = 0; i < n-1; i++) {
+//			temp += abs(arr[i] - arr[i + 1]);
+//
+//		
 //		}
-//		cout << "\n";
+//
+//		if (temp > sum) {
+//			sum = temp;
+//		}
+//
 //		return;
 //	}
 //
-//	int bk = 0;
+//
 //	for (int i = 0; i < n; i++) {
-//		if (bk != nums[i]) {
+//
+//		if (!isused[i]) {
 //			
 //			arr[a] = nums[i];
+//			isused[i] = 1;
 //			re(a + 1);
-//
-//
+//			isused[i] = 0;
 //		}
+//
+//
 //
 //	}
 //
 //
 //
+//	
 //
 //}
 //
 //
 //int main() {
+//
 //	ios::sync_with_stdio(0);
 //	cin.tie(0);
 //
-//	cin >> n >> m;
+//	cin >> n;
 //
 //	for (int i = 0; i < n; i++) {
 //		cin >> nums[i];
 //	}
 //
-//	sort(nums, nums + n );
-//
 //	re(0);
-//
-//
+//	cout << sum;
 //}
