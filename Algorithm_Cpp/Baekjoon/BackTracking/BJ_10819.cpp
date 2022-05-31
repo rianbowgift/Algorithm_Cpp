1f52d61480c39271c3541bd5352ad409004654d0
YakultLover <rianbowgift@gmail.com>
2022-06-01 01:31:17 +0900

    감잡히는 백트렉킹

---
+++ Algorithm_Cpp/Baekjoon/BackTracking/BJ_10819.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+int n;
+int arr[10];
+bool isused[10];
+int sum;
+int nums[10];
+
+
+void re(int a) {
+	if (a == n) {
+		int temp = 0;
+		for (int i = 0; i < n-1; i++) {
+			temp += abs(arr[i] - arr[i + 1]);
+
+		
+		}
+
+		if (temp > sum) {
+			sum = temp;
+		}
+
+		return;
+	}
+
+
+	for (int i = 0; i < n; i++) {
+
+		if (!isused[i]) {
+			
+			arr[a] = nums[i];
+			isused[i] = 1;
+			re(a + 1);
+			isused[i] = 0;
+		}
+
+
+
+	}
+
+
+
+	
+
+}
+
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> nums[i];
+	}
+
+	re(0);
+	cout << sum;
+}
--- Algorithm_Cpp/Baekjoon/Failure/BJ_15663.cpp
+++ Algorithm_Cpp/Baekjoon/Failure/BJ_15663.cpp
@@ -9,72 +9,29 @@
 //int arr[100];
 //bool isused[10];
 //int nums[100];
-//int counter;
-//vector<int> tr_num;
+//
 //
 //void re(int a) {
-//	if (m == a) {
-//		string temp;
-//		for (int i = 0; i < m; i++) {
-//			temp += to_string(arr[i]);
-//		}
-//
-//		if (!tr_num.empty()) {
-//			vector<int> tr_temp = tr_num;
-//			int size = to_string(tr_num.back()).size();	// 마지막 배열원소 길이
-//			int number = 0;	//마지막 두번째 숫자위치 
-//			if (size < 2) {
-//				number = 0;
-//			}
-//			else {
-//				number = to_string(tr_num.back())[size - 2];
-//			}
-//				
-//			while (true) {
-//				if (to_string(tr_num.back())[number]) {
-//
-//				}
-//
-//				if (tr_num.back() == stoi(temp)) {
-//					return;
-//				}
-//				else {
-//					tr_num.pop_back();
-//				}
-//			}
-//
-//
-//
-//
-//
-//
-//
-//		
-//	
-//		}
-//
-//
+//	if (a == m) {
 //		for (int i = 0; i < m; i++) {
 //			cout << arr[i] << ' ';
 //		}
-//		tr_num.push_back(stoi(temp));
 //		cout << "\n";
 //		return;
 //	}
 //
-//	for (int i = 1; i <= n; i++) {
-//
-//		if (!isused[i]) {
-//
+//	int bk = 0;
+//	for (int i = 0; i < n; i++) {
+//		if (bk != nums[i]) {
+//			
 //			arr[a] = nums[i];
-//			isused[i] = 1;
 //			re(a + 1);
-//			isused[i] = 0;
 //
 //
 //		}
 //
 //	}
+//
 //
 //
 //
@@ -87,11 +44,11 @@
 //
 //	cin >> n >> m;
 //
-//	for (int i = 1; i <= n; i++) {
+//	for (int i = 0; i < n; i++) {
 //		cin >> nums[i];
 //	}
 //
-//	sort(nums + 1, nums + n + 1);
+//	sort(nums, nums + n );
 //
 //	re(0);
 //
