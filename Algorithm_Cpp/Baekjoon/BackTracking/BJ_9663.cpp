1b24f0feff6cc9812607134b769929b414e24392
YakultLover <rianbowgift@gmail.com>
2022-06-06 13:20:25 +0900

    알수없는 시간초과

--- Algorithm_Cpp/Baekjoon/BackTracking/BJ_9663.cpp
+++ Algorithm_Cpp/Baekjoon/BackTracking/BJ_9663.cpp
@@ -19,7 +19,7 @@ void re(int a) {
 
 
 
-	for (int i = 0; i < n; i++) {
+	for (int i = a; i < n; i++) {
 		
 		for (int j = 0; j < n; j++) {
 			bool ck = false;
@@ -34,30 +34,42 @@ void re(int a) {
 						break;
 					}
 
-					if(isused[i][j])
 
-					//if (isused[i-k][j+k]) {
-					//	ck = true;
-					//	break;
-					//}
-					//if (isused[i+k][j+k]) {
-					//	ck = true;
-					//	break;
-					//}
-					//if (isused[i+k][j-k]) {
-					//	ck = true;
-					//	break;
-					//}
-					//if (isused[i-k][j-k]) {
-					//	ck = true;
-					//	break;
-					//}
+					if (0<=i-k && j+k<n ) {		//1사분면
+						if (isused[i - k][j + k]) {
+							ck = true;
+							break;
+						}
+					}
+					if (i + k<n && j + k < n) {	//4사분면
+						if (isused[i + k][j + k]) {
+							ck = true;
+							break;
+						}
+					}
+					if (i + k<n && 0<=j - k) { //3사분면
+						if (isused[i + k][j - k]) {
+							ck = true;
+							break;
+						}
+					}
+					if (0 <= i - k && 0<=j - k) { //2사분면
+						if (isused[i - k][j - k]) {
+							ck = true;
+							break;
+						}
+					}
 					
 				}
 
 				if (ck == true) {
+					if (j == n-1) {
+						return;
+					}
 					continue;
 				}
+
+
 
 				isused[i][j] = 1;
 				re(a + 1);
