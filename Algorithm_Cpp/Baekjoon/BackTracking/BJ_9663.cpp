bf58d234a1b4619f626217911570f6a2de5a4b67
YakultLover <rianbowgift@gmail.com>
2022-06-06 20:49:19 +0900

    테스트

--- Algorithm_Cpp/Baekjoon/BackTracking/BJ_9663.cpp
+++ Algorithm_Cpp/Baekjoon/BackTracking/BJ_9663.cpp
@@ -1,102 +1,102 @@
-#include<iostream>
-using namespace std;
- 
-int n;
-
-int arr[20][20];
-int isused[20][20];
-int sum;
-
-
-void re(int a) {
-
-	if (a == n) {
-		
-		sum++;
-		return;
-	}
-	
-
-
-
-	for (int i = a; i < n; i++) {
-		
-		for (int j = 0; j < n; j++) {
-			bool ck = false;
-			if (!isused[i][j]) {
-				for (int k = 0; k < n; k++) {
-					if (isused[i][k]) {
-						ck = true;
-						break;
-					}
-					if (isused[k][j]) {
-						ck = true;
-						break;
-					}
-
-
-					if (0<=i-k && j+k<n ) {		//1사분면
-						if (isused[i - k][j + k]) {
-							ck = true;
-							break;
-						}
-					}
-					if (i + k<n && j + k < n) {	//4사분면
-						if (isused[i + k][j + k]) {
-							ck = true;
-							break;
-						}
-					}
-					if (i + k<n && 0<=j - k) { //3사분면
-						if (isused[i + k][j - k]) {
-							ck = true;
-							break;
-						}
-					}
-					if (0 <= i - k && 0<=j - k) { //2사분면
-						if (isused[i - k][j - k]) {
-							ck = true;
-							break;
-						}
-					}
-					
-				}
-
-				if (ck == true) {
-					if (j == n-1) {
-						return;
-					}
-					continue;
-				}
-
-
-
-				isused[i][j] = 1;
-				re(a + 1);
-				isused[i][j] = 0; 
-			}
-
-
-
-		}
-
-
-	}
-
-
-
-}
-
-
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	cin >> n;
-
-	re(0);
-	cout << sum;
-
-
-}
+//#include<iostream>
+//using namespace std;
+// 
+//int n;
+//
+//int arr[20][20];
+//int isused[20][20];
+//int sum;
+//
+//
+//void re(int a) {
+//
+//	if (a == n) {
+//		
+//		sum++;
+//		return;
+//	}
+//	
+//
+//
+//
+//	for (int i = a; i < n; i++) {
+//		
+//		for (int j = 0; j < n; j++) {
+//			bool ck = false;
+//			if (!isused[i][j]) {
+//				for (int k = 0; k < n; k++) {
+//					if (isused[i][k]) {
+//						ck = true;
+//						break;
+//					}
+//					if (isused[k][j]) {
+//						ck = true;
+//						break;
+//					}
+//
+//
+//					if (0<=i-k && j+k<n ) {		//1사분면
+//						if (isused[i - k][j + k]) {
+//							ck = true;
+//							break;
+//						}
+//					}
+//					if (i + k<n && j + k < n) {	//4사분면
+//						if (isused[i + k][j + k]) {
+//							ck = true;
+//							break;
+//						}
+//					}
+//					if (i + k<n && 0<=j - k) { //3사분면
+//						if (isused[i + k][j - k]) {
+//							ck = true;
+//							break;
+//						}
+//					}
+//					if (0 <= i - k && 0<=j - k) { //2사분면
+//						if (isused[i - k][j - k]) {
+//							ck = true;
+//							break;
+//						}
+//					}
+//					
+//				}
+//
+//				if (ck == true) {
+//					if (j == n-1) {
+//						return;
+//					}
+//					continue;
+//				}
+//
+//
+//
+//				isused[i][j] = 1;
+//				re(a + 1);
+//				isused[i][j] = 0; 
+//			}
+//
+//
+//
+//		}
+//
+//
+//	}
+//
+//
+//
+//}
+//
+//
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	cin >> n;
+//
+//	re(0);
+//	cout << sum;
+//
+//
+//}
---
+++ Algorithm_Cpp/ETC/cppprec.cpp
@@ -0,0 +1,28 @@
+//#include<iostream>
+//#include<stack>
+//using namespace std;
+//
+//int main() {
+//
+//	stack<int> st;
+//	st.push(1);
+//	cout << st.top();
+//	//st.top();
+//	while (!st.empty()) {
+//		
+//		int temp = st.top();
+//		cout << temp;
+//
+//		st.pop();
+//
+//
+//	}
+//	
+//
+//	int a;
+//	string c;
+//	cin >> a;
+//
+//	cout << a;
+//	
+//}
