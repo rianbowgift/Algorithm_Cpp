3a2e7e1045ada476767111e839f805eff761afc8
YakultLover <rianbowgift@gmail.com>
2022-06-08 19:16:51 +0900

    생각한다 잘못된 번역

--- Algorithm_Cpp/Baekjoon/Stack/BJ_3986.cpp
+++ Algorithm_Cpp/Baekjoon/Stack/BJ_3986.cpp
@@ -1,38 +1,60 @@
-#include<iostream>
-#include<stack>
-#include<string>
-using namespace std;
-
-int main() {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	int n;
-
-	cin >> n;
-	int count = 0;
-
-	for (int k = 0; k < n; k++) {
-		string temp;
-		cin >> temp;
-
-		stack<char> st;
-		bool sw = true;
-
-		if (temp.size() % 2 != 0) {//È¦¼ö¸é Á¦¿Ü 
-			continue;
-		}
-		
-		int sized = temp.size();
-
-		for(int i=0;)
-
-
-	}
-
-	cout << count;
-
-
-
-}
+//#include<iostream>
+//#include<stack>
+//#include<string>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int n;
+//
+//	cin >> n;
+//	int count = 0;
+//
+//	for (int k = 0; k < n; k++) {
+//		string temp;
+//		cin >> temp;
+//
+//		if (temp.size() % 2 != 0) {//È¦¼ö¸é Á¦¿Ü 
+//			continue;
+//		}
+//
+//		stack<char> st;
+//
+//		for (int i = 0; i < temp.size(); i++) {
+//			if (!st.empty()) {
+//
+//				if (st.top() == temp[i]) {
+//					st.pop();
+//					continue;
+//				}
+//				else {
+//					st.push(temp[i]);
+//					
+//				}
+//
+//
+//			}
+//			else {
+//				st.push(temp[i]);
+//			}
+//		}
+//
+//		if (st.empty()) {
+//			count++;
+//		}
+//
+//
+//		
+//	
+//
+//
+//	}
+//
+//	cout << count;
+//
+//
+//
+//}
