92a27be30152374c10d7612116126dbaab061492
YakultLover <rianbowgift@gmail.com>
2022-06-11 03:32:11 +0900

    연습

--- Algorithm_Cpp/Baekjoon/Failure/BJ_10799.cpp
+++ Algorithm_Cpp/Baekjoon/Failure/BJ_10799.cpp
@@ -7,37 +7,8 @@
 //	ios::sync_with_stdio(0);
 //	cin.tie(0);
 //
-//	string s;
-//	cin >> s;
 //	stack<char> st;
 //
-//
-//	for (int i = 0; i < s.size(); i++) {
-//
-//		if (s[i] == '(') {
-//			st.push(s[i]);
-//		}
-//		else {
-//
-//
-//			while ) {
-//
-//			}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//		}
-//
-//
-//
-//
-//	}
 //
 //
 //
