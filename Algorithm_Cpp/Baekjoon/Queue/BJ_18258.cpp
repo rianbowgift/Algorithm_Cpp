b3f917f0b1b0e357e45d5b2e99a1c1b0fb9ec395
YakultLover <rianbowgift@gmail.com>
2022-06-13 20:22:10 +0900

    간단히넘김

---
+++ Algorithm_Cpp/Baekjoon/Queue/BJ_18258.cpp
@@ -0,0 +1,67 @@
+//#include<iostream>
+//#include<queue>
+//using namespace std;
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	queue<int> arr;
+//	int a;
+//	cin >> a;
+//
+//	for (int i = 0; i < a; i++) {
+//		string temp;
+//		cin >> temp;
+//
+//		if (temp == "push") {
+//			int temp;
+//			cin >> temp;
+//			arr.push(temp);
+//		}
+//		else if (temp == "pop") {
+//			if (arr.empty()) {
+//				cout << "-1" << "\n";
+//			}
+//			else {
+//				cout << arr.front() << "\n";
+//				arr.pop();
+//			}
+//		}
+//		else if (temp == "size") {
+//			cout << arr.size() << "\n";
+//		}
+//		else if (temp == "empty") {
+//			if (arr.empty()) {
+//				cout << "1" << "\n";
+//			}
+//			else {
+//				cout << "0" << "\n";
+//			}
+//		}
+//		else if (temp == "front") {
+//			if (arr.empty()) {
+//				cout << "-1" << "\n";
+//			}
+//			else {
+//				cout << arr.front() << "\n";
+//			}
+//
+//		}
+//		else if (temp == "back") {
+//			if (arr.empty()) {
+//				cout << "-1" << "\n";
+//			}
+//			else {
+//				cout << arr.back() << "\n";
+//			}
+//
+//		}
+//
+//	}
+//
+//
+//
+//
+//	return 0;
+//}
