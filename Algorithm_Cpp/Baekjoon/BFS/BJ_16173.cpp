1abe80ce39ae219f070080744a0f0d805ca2ee13
YakultLover <rianbowgift@gmail.com>
2022-06-20 19:29:26 +0900

    잠시보류

--- Algorithm_Cpp/Baekjoon/BFS/BJ_16173.cpp
+++ Algorithm_Cpp/Baekjoon/BFS/BJ_16173.cpp
@@ -1,19 +1,19 @@
-#include<iostream>
-using namespace std;
-
-int board[500][500];
-int isused[500][500];
-
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
-
-int n, m;
-
-int main() {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	
-
-}
+//#include<iostream>
+//using namespace std;
+//
+//int board[500][500];
+//int isused[500][500];
+//
+//int dx[4] = {1,0,-1,0};
+//int dy[4] = {0,1,0,-1};
+//
+//int n, m;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	
+//
+//}
