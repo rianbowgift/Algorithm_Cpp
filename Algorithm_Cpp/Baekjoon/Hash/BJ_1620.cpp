ce929a9857500d7c6d2da7e9b6733244ba12fe7d
YakultLover <rianbowgift@gmail.com>
2022-06-21 19:03:15 +0900

    단순하게 생각하면 좀더 쉬움

--- Algorithm_Cpp/Baekjoon/Hash/BJ_1620.cpp
+++ Algorithm_Cpp/Baekjoon/Hash/BJ_1620.cpp
@@ -1,11 +1,42 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-
-
-}
+//#include<iostream>
+//#include<map>
+//#include<string>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	map<string, string> hash;
+//	map<string, string > hash2;
+//
+//	int n,m;
+//	cin >> n >> m;
+//
+//	for (int i = 0; i < n; i++) {
+//
+//		string temp;
+//		cin >> temp;
+//		hash[temp] = to_string(i+1);
+//		hash2[to_string(i + 1)] = temp;
+//	}
+//
+//
+//	for (int i = 0; i < m; i++) {
+//		
+//		string temp;
+//		cin >> temp;
+//		if (temp[0] >= 'A' && temp[0] <= 'Z') {	//문자입력이라면
+//			cout << hash[temp];
+//		}
+//		else {	//숫자입력이라면
+//			cout << hash2[temp];
+//		}
+//		cout << "\n";
+//
+//	}
+//
+//	
+//
+//
+//}
