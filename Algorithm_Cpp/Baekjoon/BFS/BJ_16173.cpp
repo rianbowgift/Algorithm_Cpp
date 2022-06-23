98aa32ed2dd07b1400c2cea00ee9a0ce85e77ca2
YakultLover <rianbowgift@gmail.com>
2022-06-23 15:18:35 +0900

    계산식 이용? 오히려 어려움

--- Algorithm_Cpp/Baekjoon/BFS/BJ_16173.cpp
+++ Algorithm_Cpp/Baekjoon/BFS/BJ_16173.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int board[500][500];
 int vis[500][500];
 
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
+int dx[4] = {1,0};
+int dy[4] = {0,1};
 
 int n, m;
 
@@ -14,6 +14,16 @@ int main() {
 
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			
+			cin >> board[i][j];
+		}
+	}
+
 
 	queue<pair<int, int>> Q;
 	vis[0][0] = 1;
@@ -25,21 +35,34 @@ int main() {
 		pair<int, int> cur = Q.front();
 		Q.pop();
 
-		for (int dir = 0; dir < 4; dir++) {
-			int nx = cur.first + dx[dir];
-			int ny = cur.second + dy[dir];
+		int maxd = board[cur.first][cur.second];
+		
+		for (int dir = 0; dir < 2; dir++) {
+			int nx = cur.first + maxd;
+			
 
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-				continue;
-			}
-			if (vis[nx][ny] || board[nx][ny] != 1) {
-				continue;
-			}
-
-			vis[nx][ny] = 1;
-			Q.push({ nx,ny });
 
 		}
+		
+
+
+
+
+		//for (int dir = 0; dir < 4; dir++) {
+		//	int nx = cur.first + dx[dir];
+		//	int ny = cur.second + dy[dir];
+
+		//	if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+		//		continue;
+		//	}
+		//	if (vis[nx][ny] || board[nx][ny] != 1) {
+		//		continue;
+		//	}
+
+		//	vis[nx][ny] = 1;
+		//	Q.push({ nx,ny });
+
+		//}
 
 
 
