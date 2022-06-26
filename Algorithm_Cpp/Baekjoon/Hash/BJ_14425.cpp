43ad523281d34103bd5b88e802158085877541ab
YakultLover <rianbowgift@gmail.com>
2022-06-27 00:15:38 +0900

    간단하게 컷

--- Algorithm_Cpp/Baekjoon/Hash/BJ_14425.cpp
+++ Algorithm_Cpp/Baekjoon/Hash/BJ_14425.cpp
@@ -1,12 +1,31 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-
-
-
-}
+//#include<iostream>
+//#include<vector>
+//#include<map>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int n, m;
+//	map<string,int> mp;
+//	int count = 0;
+//	cin >> n >> m;
+//
+//	for (int i = 0; i < n; i++) {
+//		string temp;
+//		cin >> temp;
+//		mp[temp] = 1;
+//
+//	}
+//
+//	for (int i = 0; i < m; i++) {
+//		string temp;
+//		cin >> temp;
+//		if (mp[temp] == 1) {
+//			count++;
+//		}
+//	}
+//	cout << count;
+//}
