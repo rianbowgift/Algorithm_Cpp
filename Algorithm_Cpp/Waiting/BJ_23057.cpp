10b036de416b6bbb765c9ffe5c2e348023bc3991
YakultLover <rianbowgift@gmail.com>
2022-06-27 17:54:38 +0900

    해쉬 문제?

---
+++ Algorithm_Cpp/Waiting/BJ_23057.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int n;
+//	cin >> n;
+//
+//	for (int i = 0; i < n; i++) {
+//
+//		
+//
+//	}
+//
+//	4
+//		1 3 6 9 = 19
+//		
+//		2 5 8 11 14 17 
+//
+//	2 45 78
+//
+//}
