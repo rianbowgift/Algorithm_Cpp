400f1b8c4b1f7898fa89bcdcd80a3cef1c7b8036
YakultLover <rianbowgift@gmail.com>
2022-06-27 17:09:14 +0900

    해쉬보단 수학에 가까웠음

---
+++ Algorithm_Cpp/Baekjoon/Hash/BJ_9375.cpp
@@ -0,0 +1,52 @@
+//#include<iostream>
+//#include<map>
+//#include<string>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int n, m;
+//	cin >> n;
+//	
+//	for (int z = 0; z < n; z++) {
+//
+//		cin >> m;
+//		
+//		map<string,int> mp;
+//		int count = 0;
+//
+//		for (int x = 0; x < m; x++) {
+//
+//			string temp;
+//			string type;
+//			cin >> temp >> type;
+//
+//
+//			mp[type] += 1;
+//		
+//
+//		}
+//		int sum = 1;
+//		for (auto temp : mp) {
+//			
+//			int nums = temp.second + 1;
+//			
+//			sum *= nums;
+//		}
+//
+//		sum--;
+//		cout << sum << "\n";
+//			
+//
+//
+//
+//
+//
+//
+//	}
+//
+//
+//}
