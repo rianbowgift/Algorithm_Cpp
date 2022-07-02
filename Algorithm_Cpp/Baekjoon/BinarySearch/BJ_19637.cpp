0d44b712535fa0eb224f3c71b4ee82f4436ea186
YakultLover <rianbowgift@gmail.com>
2022-07-02 18:31:28 +0900

    로직구현실패. 다시

--- Algorithm_Cpp/Baekjoon/BinarySearch/BJ_19637.cpp
+++ Algorithm_Cpp/Baekjoon/BinarySearch/BJ_19637.cpp
@@ -1,10 +1,26 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main() {
 
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	vector<pair<string, int>> grade;
+
+	int n, m;
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		string temp;
+		int temp2;
+		cin >> temp >> temp2;
+		grade.push_back({ temp,temp2 });
+
+	}
+
+
 
 
 }
