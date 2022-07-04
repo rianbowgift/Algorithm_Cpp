f3e9d022110abaaf4122457c2455af1264c6e284
YakultLover <rianbowgift@gmail.com>
2022-07-04 16:14:17 +0900

    닥터슬럼프

---
+++ Algorithm_Cpp/Baekjoon/Implementation/BJ_2420.cpp
@@ -0,0 +1,16 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	long long n, m;
+//	cin >> n >> m;
+//
+//	cout << abs(n - m);
+//
+//
+//}
