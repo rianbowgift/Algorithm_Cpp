4cc278b6d7173446a8a8a9d484fdca3d66d166f3
YakultLover <rianbowgift@gmail.com>
2022-07-05 14:39:10 +0900

    수학문제

--- Algorithm_Cpp/Baekjoon/BinarySearch/BJ_19637.cpp
+++ Algorithm_Cpp/Baekjoon/BinarySearch/BJ_19637.cpp
@@ -1,20 +1,20 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
-int main() {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	vector<pair<string, int>> grade;
-
-	int n, m;
-
-	cin >> n >> m;
-
-
-
-
-
-
-}
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	vector<pair<string, int>> grade;
+//
+//	int n, m;
+//
+//	cin >> n >> m;
+//
+//
+//
+//
+//
+//
+//}
---
+++ Algorithm_Cpp/Baekjoon/Math/BJ_1009.cpp
@@ -0,0 +1,35 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int count;
+//	cin >> count;
+//	int n, m;
+//
+//	
+//	for (int i = 0; i < count; i++) {
+//
+//		cin >> n >> m;
+//		int sum = 1;
+//
+//		while (m != 0) {
+//			sum = (sum * n) % 10;
+//			m--;
+//		}
+//
+//
+//
+//		if (sum == 0) {
+//			cout << "10" << "\n";
+//		}
+//		else {
+//			cout << sum << "\n";
+//		}
+//
+//		
+//	}
+//}
