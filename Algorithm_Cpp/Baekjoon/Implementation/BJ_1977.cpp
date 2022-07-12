7488eb2f081fa76bfd83a3e64b17adf0f7f10b1f
YakultLover <rianbowgift@gmail.com>
2022-07-12 09:46:57 +0900

    sqrt 사용시 cmath필요

---
+++ Algorithm_Cpp/Baekjoon/Implementation/BJ_1977.cpp
@@ -0,0 +1,38 @@
+//#include<iostream>
+//#include<cmath>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int n, m;
+//	int sum = 0;
+//	int min = 0;
+//	cin >> n >> m;
+//
+//	double x = sqrt(n);
+//	n = sqrt(n);
+//	m = sqrt(m);
+//
+//	if (n == m) {
+//		cout << "-1";
+//		return 0;
+//	}
+//
+//	if (x != n) {
+//		n++;
+//	}
+//
+//
+//
+//	min = n * n;
+//
+//	for (int i = n; i <= m; i++) {
+//		sum += i * i;
+//	}
+//
+//	cout << sum << "\n" << min;
+//
+//}
