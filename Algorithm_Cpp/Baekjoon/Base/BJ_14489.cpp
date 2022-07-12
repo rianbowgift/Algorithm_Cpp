03fe20de802fd18945d330ba3b5e79caa3920ada
YakultLover <rianbowgift@gmail.com>
2022-07-13 00:39:27 +0900

    기본연산 간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_14489.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int a, b, c;
+//	cin >> a >> b >> c;
+//
+//	if (a + b >= c * 2) {
+//		cout << (a + b) - c * 2;
+//	}
+//	else {
+//		cout << a + b;
+//	}
+//
+//
+//}
