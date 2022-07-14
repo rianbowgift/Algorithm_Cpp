aee0d2d397c15ddaf9e1f8f150b4c95a2da5debb
YakultLover <rianbowgift@gmail.com>
2022-07-15 07:28:40 +0900

    말장난

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_14645.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int n, m;
+//	cin >> n >> m;
+//
+//	for (int i = 0; i < n; i++) {
+//		int temp;
+//
+//		cin >> m >> temp;
+//	}
+//
+//	cout << "ºñ¿ÍÀÌ";
+//
+//}
