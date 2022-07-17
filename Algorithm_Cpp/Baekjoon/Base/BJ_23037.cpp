459b84eefb0cddfb40820aa54c9499e963df4f69
YakultLover <rianbowgift@gmail.com>
2022-07-18 00:05:32 +0900

    string char int 변환

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_23037.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	string a;
+//	cin >> a;
+//	int sum = 0;
+//
+//	for (int i = 0; i < 5; i++) {
+//		sum += (a[i] - '0') * (a[i] - '0')* (a[i] - '0')* (a[i] - '0')* (a[i] - '0');
+//	}
+//
+//	cout << sum;
+//
+//}
