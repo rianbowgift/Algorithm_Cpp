9bab0de11b721852ccba56113db89eb27a0b30b2
YakultLover <rianbowgift@gmail.com>
2022-07-20 02:37:23 +0900

    수학..

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_25238.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//
+//
+//	int a, b;
+//	int sum = 0;
+//	cin >> a >> b;
+//
+//	if (a == 0 || b == 0) {
+//		sum = a - b;
+//	}
+//	else {
+//		sum = a * (100 - b) / 100;
+//
+//
+//
+//	}
+//
+//	if (sum >= 100) {
+//		cout << "0";
+//	}
+//	else {
+//		cout << "1";
+//	}
+//
+//
+//	
+//
+//
+//
+//}
