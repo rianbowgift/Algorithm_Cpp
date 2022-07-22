b068aaaf7c334a80226aa352b88ab6826e028aef
YakultLover <rianbowgift@gmail.com>
2022-07-23 00:00:35 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_5597.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//using namespace std;
+//int arr[30];
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	
+//
+//	for (int i = 0; i < 28; i++) {
+//
+//		int temp;
+//		cin >> temp;
+//		arr[temp - 1] = 1;
+//
+//	}
+//
+//	for (int i = 0; i < 30; i++) {
+//		if (arr[i] == 0) {
+//			cout << i + 1 << "\n";
+//		}
+//	}
+//
+//
+//}
