2da689f4ef2b82fbd0f702a89c53a8725990240c
YakultLover <rianbowgift@gmail.com>
2022-07-24 07:02:34 +0900

    소수점 고정출력

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_2754.cpp
@@ -0,0 +1,45 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	string a;
+//	float sum = 0.0;
+//	cin >> a;
+//
+//	if (a[0] == 'A') {
+//		sum += 4;
+//	}
+//	else if (a[0] == 'B') {
+//		sum += 3;
+//
+//	}
+//	else if (a[0] == 'C') {
+//		sum += 2;
+//
+//	}
+//	else if (a[0] == 'D') {
+//		sum += 1;
+//
+//	}
+//	else {
+//		cout << "0.0";
+//		return 0;
+//	}
+//	
+//	if (a[1] == '+') {
+//		sum += 0.3;
+//	}
+//	else if (a[1] == '-') {
+//		sum -= 0.3;
+//
+//	}
+//
+//	cout.precision(1);
+//	cout << fixed;
+//	cout << sum;
+//
+//}
