4a6d1f362f26f33123342aaf01b9fe1dc5797e4e
YakultLover <rianbowgift@gmail.com>
2022-08-02 00:02:18 +0900

    biginteger 사용방법 미숙

---
+++ Algorithm_Cpp/Baekjoon/Failure/BigInteger/BJ_8437.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int a, b;
+//
+//	cin >> a >> b;
+//
+//	if (b % 2 == 0) {
+//		cout << (a / 2) + (b / 2) << "\n" << (a / 2) - (b / 2);
+//	}
+//	else {
+//		cout << (a / 2) + b << "\n" << a / 2;
+//	}
+//
+//	
+//
+//
+//}
