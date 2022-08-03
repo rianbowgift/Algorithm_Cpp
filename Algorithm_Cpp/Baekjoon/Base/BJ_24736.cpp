b4bcad651806c60389da8375573755f5e1a78766
YakultLover <rianbowgift@gmail.com>
2022-08-04 00:32:35 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_24736.cpp
@@ -0,0 +1,14 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int a, b, c, d, e;
+//	int A, B, C, D, E;
+//	cin >> a >> b >> c >> d >> e;
+//	cin >> A >> B >> C >> D >> E;
+//	cout << (a * 6) + (b * 3) + (c * 2) + (d * 1) + (e * 2) << " ";
+//	cout << (A * 6) + (B * 3) + (C * 2) + (D * 1) + (E * 2);
+//
+//
+//}
