8e4e5bbafc17d77a6f8288adca0b1346c679bee3
YakultLover <rianbowgift@gmail.com>
2022-08-16 01:03:26 +0900

    %나머지 연산 기초응용

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_5532.cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int a, b, c, d, e;
+//	int x;
+//	cin >> a >> b >> c >> d >> e;
+//	
+//	if (b / d > c / e) {
+//		if (b % d == 0) {
+//			x = b / d;
+//		}
+//		else {
+//			x = (b / d) + 1;
+//		}
+//	}
+//	else {
+//		if (c % e == 0) {
+//			x = c / e;
+//		}
+//		else {
+//			x = (c / e) + 1;
+//		}
+//	}
+//
+//	cout << a-x;
+//	 
+//
+//
+//}
