128bcd49e3e4abbffe152cf2ed5c5c08cb35fe51
YakultLover <rianbowgift@gmail.com>
2022-08-20 03:57:17 +0900

    sort기능 활용하면 코드간결++

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_11948.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//
+//int main() {
+//	int arr[4];
+//	int max2 = 0;
+//	for (int i = 0; i < 4; i++) {
+//		cin >> arr[i];
+//	}
+//	sort(arr, arr + 4);
+//	for (int i = 0; i < 2; i++) {
+//		int temp;
+//		cin >> temp;
+//		if (temp > max2) {
+//			max2 = temp;
+//		}
+//	}
+//
+//	cout << arr[1]+arr[2]+arr[3]+ max2;
+//
+//}
