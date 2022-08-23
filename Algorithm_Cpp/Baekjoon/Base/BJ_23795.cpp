78581c5866b394966491aa8f197ccd042618667a
YakultLover <rianbowgift@gmail.com>
2022-08-24 00:27:51 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_23795.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int sum = 0;
+//
+//	while (true) {
+//		int temp;
+//		cin >> temp;
+//		if (temp == -1) {
+//			break;
+//		}
+//		sum += temp;
+//}
+//	cout << sum;
+//
+//
+//}
