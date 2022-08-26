cc06e764e2e688015bb979923577965236337e15
YakultLover <rianbowgift@gmail.com>
2022-08-27 02:10:01 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_15726.cpp
@@ -0,0 +1,18 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	double a, b, c;
+//	int sum = 0;
+//	cin >> a >> b >> c;
+//	
+//	if (a * b / c > sum) {
+//		sum = a * b / c;
+//	}
+//	if (a / b * c > sum) {
+//		sum = a / b * c;
+//	}
+//	cout << sum;
+//
+//}
