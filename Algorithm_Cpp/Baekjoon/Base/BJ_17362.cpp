af931a72631626a7d8a40fa8435af09f02606236
YakultLover <rianbowgift@gmail.com>
2022-08-29 00:00:12 +0900

    생각보단 어려운 간단한문제

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_17362.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int a;
+//
+//	cin >> a;
+///
+//	if (a % 8 == 1) {
+//		cout << "1";
+//	}
+//	else if (a % 8 == 2 || a % 8 == 0) {
+//		cout << "2";
+//	}
+//	else if (a % 8 == 3 || a % 8 == 7) {
+//		cout << "3";
+//	}
+//	else if (a % 8 == 4 || a%8 == 6) {
+//		cout << "4";
+//	}
+//	else if (a % 8 == 5) {
+//		cout << "5";
+//	}
+//
+//
+//}
