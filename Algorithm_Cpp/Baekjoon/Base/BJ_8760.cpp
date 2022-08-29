9d97d8b86716e547cb6eb357970ad033dc7f5e0f
YakultLover <rianbowgift@gmail.com>
2022-08-30 00:07:08 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_8760.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int a;
+//	cin >> a;
+//	for (int i = 0; i < a; i++) {
+//		int temp, temp2;
+//		cin >> temp >> temp2;
+//
+//		if (temp * temp2 < 2) {
+//			cout << "0" << "\n";
+//		}
+//		else {
+//			cout << temp * temp2 / 2 << "\n";
+//		}
+//	}
+//}
