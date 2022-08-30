83d5f7c44684951c6a20e18624b5076e5847b851
YakultLover <rianbowgift@gmail.com>
2022-08-31 07:17:06 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_2525.cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int a, b, c;
+//	int temp = -1;
+//
+//	cin >> a >> b >> c;
+//	
+//	b += c;
+//	while (b >= 60) {
+//		b -= 60;
+//		a++;
+//	}
+//	
+//	while (a >=24) {
+//		a--;
+//		temp++;
+//	}
+//	
+//	if (temp >= 0) {
+//		cout << temp << " " << b;
+//	}
+//	else {
+//		cout << a << " " << b;
+//	}
+//	
+//
+//
+//}
