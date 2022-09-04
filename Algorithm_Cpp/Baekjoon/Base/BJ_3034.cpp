ed00dc39a817af568c08618d414fb14880ff3a42
YakultLover <rianbowgift@gmail.com>
2022-09-05 04:20:27 +0900

    간단한 sqrt 사용

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_3034.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//
+//int main() {
+//
+//	int a, b, c;
+//	cin >> a >> b >> c;
+//	double square = sqrt((b * b) + (c * c));
+//
+//	for (int i = 0; i < a; i++) {
+//		
+//		double temp;
+//		cin >> temp;
+//		if (temp <= square) {
+//			cout << "DA" << "\n";
+//		}
+//		else {
+//			cout << "NE" << "\n";
+//		}
+//
+//	}
+//
+//
+//
+//}
