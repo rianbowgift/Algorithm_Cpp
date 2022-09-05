0e54f154f8226dd5af121510cc77a93e4f77e0e1
YakultLover <rianbowgift@gmail.com>
2022-09-06 02:50:30 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_2562.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int pos = 1;
+//	int max = 0;
+//
+//	for (int i = 1; i <= 9; i++) {
+//		int temp;
+//		cin >> temp;
+//		if (temp > max) {
+//			max = temp;
+//			pos = i;
+//		}
+//	}
+//
+//	cout << max << "\n" << pos;
+//
+//
+//}
