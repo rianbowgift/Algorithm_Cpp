54939e6ac0adc8677632e77f7db8a46c9e79118f
YakultLover <rianbowgift@gmail.com>
2022-09-08 12:32:04 +0900

    간단히(문제 정독 필요)

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_14720.cpp
@@ -0,0 +1,30 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//int main() {
+//
+//	int a;
+//	cin >> a;
+//	int shop = 0;
+//	int max = 0;
+//	
+//	for (int i = 0; i < a; i++) {
+//		int temp;
+//		cin >> temp;
+//		if (temp == shop) {
+//			shop++;
+//			max++;
+//			if (shop == 3) {
+//				shop = 0;
+//			}
+//			
+//		}
+//	
+//	}
+//
+//	cout << max;
+//	
+//
+//
+//}
