a691024c338fdedcc5dd3845e603e5f750115858
YakultLover <rianbowgift@gmail.com>
2022-09-12 02:37:12 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_10995.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int a;
+//	cin >> a;
+//
+//	for (int i = 1; i <= a; i++) {
+//		if (i % 2 == 0) {
+//			cout << " ";
+//		}
+//
+//		for (int j = 0; j < a; j++) {
+//			cout << "* ";
+//		}
+//		cout << "\n";
+//
+//
+//	}
+//
+//
+//}
