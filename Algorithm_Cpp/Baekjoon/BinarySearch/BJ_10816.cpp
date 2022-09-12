2cec36e5fabd18098ea26e13ffd7b3b4dc3eff19
YakultLover <rianbowgift@gmail.com>
2022-09-13 03:36:16 +0900

    접근방식이 잘못된 풀이?

---
+++ Algorithm_Cpp/Baekjoon/BinarySearch/BJ_10816.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//#include<algorithm>
+//
+//using namespace std;
+//
+//int arr[20000001];
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int a;
+//	int b;
+//	cin >> a;
+//	for (int i = 0; i < a; i++) {
+//		int temp;
+//		cin >> temp;
+//		arr[temp+10000000] += 1;
+//	}
+//
+//	
+//	
+//	cin >> b;
+//	for (int i = 0; i < b; i++) {
+//
+//		int temp;
+//		cin >> temp;
+//
+//		cout << arr[temp + 10000000] << " ";
+//
+//	}
+//
+//	
+//
+//
+//}
