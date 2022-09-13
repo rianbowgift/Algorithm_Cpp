c75a644b4a7474e1de30b94abaeea9e3015b7557
YakultLover <rianbowgift@gmail.com>
2022-09-13 11:35:49 +0900

    그리디 기본기

---
+++ Algorithm_Cpp/Baekjoon/Greedy/BJ_1789.cpp
@@ -0,0 +1,34 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	long long x;
+//	cin >> x;
+//
+//	long long sum = 0;
+//	long long z = 0;
+//	while (true) {
+//		z++;
+//		if (sum > x) {
+//			break;
+//		}
+//
+//
+//		sum += z;
+//
+//		if (sum > x) {
+//			z--;
+//			break;
+//		}
+//
+//
+//
+//	}
+//
+//	cout << z;
+//
+//
+//}
