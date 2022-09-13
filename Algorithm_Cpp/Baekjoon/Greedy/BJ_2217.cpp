7888bdd96ff06e3a6d53ec22cc9fbf2b552340a2
YakultLover <rianbowgift@gmail.com>
2022-09-13 14:22:39 +0900

    실패

---
+++ Algorithm_Cpp/Baekjoon/Greedy/BJ_2217.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//
+//	int a;
+//	cin >> a;
+//	int min = 10001;
+//	for (int i = 0; i < a; i++) {
+//		int temp;
+//		cin >> temp;
+//		if (temp < min) {
+//			min = temp;
+//		}
+//
+//	}
+//
+//	cout << min * a;
+//
+//
+//}
