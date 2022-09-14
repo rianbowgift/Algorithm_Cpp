9f890f0325ca11cb4198044f52cac58d4661682e
YakultLover <rianbowgift@gmail.com>
2022-09-15 08:59:06 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/Bj_2744.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	string a;
+//	string b;
+//	cin >> a;
+//	for (int i = 0; i < a.length(); i++) {
+//		if (a[i] <= 90) {
+//			b += a[i] + 32;
+//		}
+//		else {
+//			b += a[i] - 32;
+//		}
+//	}
+//
+//	cout << b;
+//
+//
+//}
