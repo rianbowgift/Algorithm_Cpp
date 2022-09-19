f8ae211b6c5d2b19fb775fe5caec3fc3a1913b17
YakultLover <rianbowgift@gmail.com>
2022-09-20 08:36:55 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_10797.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int a;
+//	int sum = 0;
+//	cin >> a;
+//
+//	for (int i = 0; i < 5; i++) {
+//		int temp;
+//		cin >> temp;
+//		if (a == temp) {
+//			sum++;
+//		}
+//	}
+//
+//	cout << sum;
+//
+//
+//}
--- Algorithm_Cpp/Baekjoon/Base/BJ_15962.cpp
+++ Algorithm_Cpp/Baekjoon/Base/BJ_15962.cpp
@@ -1,10 +1,10 @@
 //#include<iostream>
 //using namespace std;
-//using namespace std
+
 //
 //int main() {
 //
 //	cout << "파이팅!!";
-//	cout <<"파이팅!!"
+
 //
 //}
