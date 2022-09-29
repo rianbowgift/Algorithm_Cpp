fde07727f08c233f49420c479e55a39c814f4238
YakultLover <rianbowgift@gmail.com>
2022-09-29 10:51:29 +0900

    간단히_char string 변환주의

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_15873.cpp
@@ -0,0 +1,42 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main() {
+//
+//	int x;
+//	cin >> x;
+//	string x_temp = to_string(x);
+//	string a, b;
+//	if (x_temp.size() == 4) {
+//		a = x_temp[0];
+//		a += x_temp[1];
+//		b = x_temp[2];
+//		b += x_temp[3];
+//	}
+//	else if (x_temp.size() == 3) {
+//		if (x_temp[1] == '0') {
+//			a = x_temp[0];
+//			a += x_temp[1];
+//			b = x_temp[2];
+//		}
+//		else {
+//			a = x_temp[0];
+//			b = x_temp[1];
+//			b += x_temp[2];
+//		}
+//	}
+//	else {
+//		a = x_temp[0];
+//		b = x_temp[1];
+//	}
+//	
+//	int sum = stoi(a) + stoi(b);
+//	cout << sum;
+//	
+//
+//
+//	
+//	
+//
+//
+//}
