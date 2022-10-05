e4723e00ed01b4a63ba7b40ef5d216a02b84809b
YakultLover <rianbowgift@gmail.com>
2022-10-06 00:58:52 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_2490.cpp
@@ -0,0 +1,40 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	for (int i = 0; i < 3; i++) {
+//		int zero = 0;
+//		int one = 0;
+//		for (int j = 0; j < 4; j++) {
+//			int temp;
+//			cin >> temp;
+//			if (temp == 1) {
+//				one++;
+//			}
+//			else {
+//				zero++;
+//			}
+//			
+//
+//		}
+//		if (zero == 1 && one == 3) {
+//			cout << "A";
+//		}
+//		else if (zero == 2 && one == 2) {
+//			cout << "B";
+//		}
+//		else if (zero == 3 && one == 1) {
+//			cout << "C";
+//		}
+//		else if (zero == 4 && one == 0) {
+//			cout << "D";
+//		}
+//		else if (zero == 0 && one == 4) {
+//			cout << "E";
+//		}
+//		cout << "\n";
+//	}
+//
+//
+//}
