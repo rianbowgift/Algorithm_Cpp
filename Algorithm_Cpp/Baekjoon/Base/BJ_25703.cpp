621ec784bba963869a6b53dff0d9d2434054fb2d
YakultLover <rianbowgift@gmail.com>
2022-10-12 13:02:31 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_25703.cpp
@@ -0,0 +1,34 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int a;
+//	cin >> a;
+//
+//	cout << "int a;" << "\n";
+//
+//	for (int i = 0; i < a; i++) {
+//		if (i == 0) {
+//			cout << "int *ptr = &a;" << "\n";
+//			continue;
+//		}
+//		cout << "int ";
+//		for (int j = 0; j < i+1; j++) {
+//			cout << "*";
+//		}
+//		if (i == 1) {
+//			cout << "ptr" <<i+1 <<  " = &ptr";
+//		}
+//		else {
+//			cout << "ptr" << i+1 << " = &ptr" << i;
+//		}
+//
+//		cout << ";" << "\n";
+//		
+//		
+//
+//	}
+//
+//
+//}
