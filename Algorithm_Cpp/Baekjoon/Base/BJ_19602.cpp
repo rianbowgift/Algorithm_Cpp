bbc45b26369641086aff7f62d2e1d461c3e3efbb
YakultLover <rianbowgift@gmail.com>
2022-10-14 01:46:30 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_19602.cpp
@@ -0,0 +1,18 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int a, b, c;
+//
+//	cin >> a >> b >> c;
+//
+//	int sum = (1 * a) + (2 * b) + (3 * c);
+//	if (sum >= 10) {
+//		cout << "happy";
+//	}
+//	else {
+//		cout << "sad";
+//	}
+//
+//}
