63e2e05f173e919bd0d9e689dbc349f4b69a5465
YakultLover <rianbowgift@gmail.com>
2022-10-18 00:05:14 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_24883.cpp
@@ -0,0 +1,18 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//
+//	char a;
+//
+//	cin >> a;
+//
+//	if (a == 'n' || a == 'N') {
+//		cout << "Naver D2";
+//	
+//	}
+//	else {
+//		cout << "Naver Whale";
+//	}
+//}
