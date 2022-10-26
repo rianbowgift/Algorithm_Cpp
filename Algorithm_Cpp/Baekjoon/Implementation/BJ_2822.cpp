0c20d7422bdba4e13bae91eddadded39c6fe6053
YakultLover <rianbowgift@gmail.com>
2022-10-26 09:51:45 +0900

    억지로푼것. 후에 다시풀어볼것

---
+++ Algorithm_Cpp/Baekjoon/Implementation/BJ_2822.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//#include<algorithm>
+//#include<vector>
+//using namespace std;
+//
+//vector<pair<int, int>> arr;
+//vector<int> arr2;
+//
+//
+//
+//
+//int main() {
+//	for (int i = 0; i < 8; i++) {
+//		int temp;
+//		cin >> temp;
+//		arr.push_back({temp,i+1});
+//		
+//
+//	}
+//
+//	sort(arr.begin(),arr.end());
+//
+//	cout << arr[3].first + arr[4].first + arr[5].first + arr[6].first + arr[7].first << "\n";
+//	for (int i = 3; i < 8; i++) {
+//		arr2.push_back(arr[i].second);
+//	}
+//	sort(arr2.begin(), arr2.end());
+//	for (int i = 0; i < 5; i++) {
+//		cout << arr2[i] << " ";
+//	}
+//
+//}
