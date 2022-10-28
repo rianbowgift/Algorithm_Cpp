f8be215a4cc0f0d5b0ded5f1571e14283a7eb83b
YakultLover <rianbowgift@gmail.com>
2022-10-28 13:04:38 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_3003.cpp
@@ -0,0 +1,38 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	int a, b, c, d, e, f;
+//	int aa, bb, cc, dd, ee, ff;
+//	aa = 0;
+//	bb = 0;
+//	cc = 0;
+//	dd = 0;
+//	ee = 0;
+//	ff = 0;
+//
+//	cin >> a >> b >> c >> d >> e >> f;
+//
+//	if (a != 1) {
+//		aa = 1 - a;
+//	}
+//	if (b != 1) {
+//		bb = 1 - b;
+//	}
+//	if (c != 2) {
+//		cc = 2 - c;
+//	}
+//	if (d != 2) {
+//		dd = 2 - d;
+//	}
+//	if (e != 2) {
+//		ee = 2 - e;
+//	}
+//	if (f != 8) {
+//		ff = 8 - f;
+//	}
+//
+//	cout << aa << " " << bb << " " << cc << " " << dd << " " << ee << " " << ff;
+//
+//
+//}
