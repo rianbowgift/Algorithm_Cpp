df26489fdaefbfde37ab0734050b0957ae31e7df
YakultLover <rianbowgift@gmail.com>
2022-11-05 12:58:07 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_23235.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int num = 1;
+//	while (true) {
+//
+//		int temp;
+//		cin >> temp;
+//		for (int i = 0; i < temp; i++) {
+//			int temp2;
+//			cin >> temp2;
+//		}
+//
+//		if (temp == 0) {
+//			break;
+//		}
+//		else {
+//			cout << "Case " << num << ": Sorting... done!" << "\n";
+//			num++;
+//		}
+//	}
+//
+//
+//}
