339b68915aae5b169664783f2b17223f23f778c2
YakultLover <rianbowgift@gmail.com>
2022-11-13 13:19:39 +0900

    스파코 알고리즘공부

--- Algorithm_Cpp/ETC/소스.cpp
+++ Algorithm_Cpp/ETC/소스.cpp
@@ -1,33 +1,34 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	string a;
-	int sum = 0;
-	bool re = false;
-	cin >> a;
-	char temp1;
-	temp1 = a[0];
-	for (int i = 0; i < a.length(); i++) {
-		if (a[i] != temp1) {
-			if (re == false) {
-				re = true;
-				sum++;
-			}
-		}
-		else {
-			re = false;
-		}
-
-
-
-	}
-
-	cout << sum;
-
-
-}
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//
+//using namespace std;
+//
+//vector<int> all_num;
+//
+//
+//vector<int> solution(vector<int> marbles) {
+//	vector<int> answer;
+//
+//
+//
+//
+//
+//	return answer;
+//
+//}
+//
+//
+//
+//
+//
+//
+//
+//int main() {
+//
+//
+//	vector<int> marbles = { 1,2,3,4,4 };
+//	solution(marbles);
+//
+//
+//}
