168c6b860301467fa3687314e714d21277c8b580
YakultLover <rianbowgift@gmail.com>
2022-11-19 07:38:09 +0900

    스파코 알고리즘공부

--- Algorithm_Cpp/Baekjoon/DP/BJ_11660.cpp
+++ Algorithm_Cpp/Baekjoon/DP/BJ_11660.cpp
@@ -7,6 +7,8 @@ int arr[1050][1050];
 vector<vector<pair<int, int>>>sum;
 //int sum[10000][pair<int,int>];
 
+
+
 int main() {
 
 	int n,m;
@@ -18,12 +20,63 @@ int main() {
 		}
 	}
 
-	//0번배열 채우기
+
+
+
+
+
+	//y 가로0번쨰 채우기
+
+
+	
+
+	//초기값 채우기
+
+	//이중백터에 값을넣을때 순서!!
+
+	//1.arr[0]을 만들어준다
+	sum.push_back({});
+	//2.arr[0][0]을 만들어준다
+	sum[0].push_back({});
+	//3.arr[0][0]에 값을 넣어준다(실제 값을넣는 단계)
+	sum[0][0].first = arr[0][0];
+	sum[0][0].second = arr[0][0];
+
+	//1번과 2번단계를 꼭 해야한다(안하면 오류)
+
+
+
+	
+	//가로줄 채우기 //시간복잡도 n^2
 	for (int i = 0; i < n; i++) {
-		vector<pair<int, int>> temp;
+		sum.push_back({});
 
-		sum.push_back(temp);
+		for (int j = 1; j < n; j++) {
+			sum[i].push_back({});
+			int second = sum[i][j - 1].second + arr[i][j];
+			sum[i][j].second = second;
+		}
+	}
 
+	for (int i = 0; i < n; i++) {
+		for (int j = 1; j < n; j++) {
+			int first = sum[j-1][i].first + arr[j][i];
+			sum[i][j].first = first;
+		}
+	}
+
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << sum[i][j].first << " ";
+		}
+		cout << "\n";
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << sum[i][j].second << " ";
+		}
+		cout << "\n";
 	}
 
 
