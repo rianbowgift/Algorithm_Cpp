f8fa81cea0164922562894a94e4d4d460597184b
YakultLover <rianbowgift@gmail.com>
2022-11-25 20:00:54 +0900

    간단히

--- Algorithm_Cpp/Baekjoon/Base/BJ_6810.cpp
+++ Algorithm_Cpp/Baekjoon/Base/BJ_6810.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-    
-    int a, b, c;
-    cin >> a >> b >> c;
-    cout << "The 1-3-sum is " << 91+a*1+b*3+c*1;
-
-
-}
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//    
+//    int a, b, c;
+//    cin >> a >> b >> c;
+//    cout << "The 1-3-sum is " << 91+a*1+b*3+c*1;
+//
+//
+//}
