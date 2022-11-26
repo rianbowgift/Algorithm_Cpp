2caec1c7e0db1f281c6466fb8abe904fd08cf712
YakultLover <rianbowgift@gmail.com>
2022-11-27 01:34:45 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_25784.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//int main() {
+//
+//	int arr[3];
+//	cin >> arr[0] >> arr[1] >> arr[2];
+//	sort(arr, arr + 3);
+//
+//	if (arr[0] + arr[1] == arr[2]) {
+//		cout << 1;
+//	}
+//	else if (arr[0] * arr[1] == arr[2]) {
+//		cout << 2;
+//	}
+//	else {
+//		cout << 3;
+//	}
+//
+//
+//
+//}
