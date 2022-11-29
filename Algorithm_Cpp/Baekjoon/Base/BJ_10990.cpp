b8a8d74ba201d25b0ba9507fcf1afc112155d88b
YakultLover <rianbowgift@gmail.com>
2022-11-29 11:37:15 +0900

    막코딩 별찍기

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_10990.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//
+//	int n;
+//	cin >> n;
+//
+//
+//	for (int i = 1; i <= n; i++) {
+//		if (i == 1) {
+//			for (int q = 0; q < n - 1; q++) {
+//				cout << " ";
+//			}
+//			cout << "*\n";
+//			continue;
+//		}
+//		
+//		for (int w = n-i+1; 1 < w; w--) {
+//			cout << " ";
+//		}
+//		cout << "*";
+//		for (int j = 0; j < i * 2 - 3; j++) {
+//			cout << " ";
+//		}
+//		cout << "*\n";
+//		
+//	}
+//
+//
+//}
