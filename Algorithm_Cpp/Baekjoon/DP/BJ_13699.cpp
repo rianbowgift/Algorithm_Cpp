c9b0fb52d02f3600325f18e20723c64e41fd25af
YakultLover <rianbowgift@gmail.com>
2022-12-01 12:29:10 +0900

    간단한 DP

--- Algorithm_Cpp/Baekjoon/DP/BJ_13699.cpp
+++ Algorithm_Cpp/Baekjoon/DP/BJ_13699.cpp
@@ -1,27 +1,27 @@
-#include<iostream>
-using namespace std;
-
-long long arr[500];
-
-int main() {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	int n;
-
-	cin >> n;
-
-	arr[0] = 1;
-
-	for (int i = 1; i <= n; i++) {
-		long long temp = 0;
-		for (int x = 0; x < i; x++) {
-			temp += arr[x] * arr[i - x-1];
-		}
-		arr[i] = temp;
-	}
-
-	cout << arr[n];
-
-}
+//#include<iostream>
+//using namespace std;
+//
+//long long arr[500];
+//
+//int main() {
+//
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	int n;
+//
+//	cin >> n;
+//
+//	arr[0] = 1;
+//
+//	for (int i = 1; i <= n; i++) {
+//		long long temp = 0;
+//		for (int x = 0; x < i; x++) {
+//			temp += arr[x] * arr[i - x-1];
+//		}
+//		arr[i] = temp;
+//	}
+//
+//	cout << arr[n];
+//
+//}
---
+++ Algorithm_Cpp/Baekjoon/DP/BJ_14495.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//using namespace std;
+//
+//long long arr[200];
+//
+//int main() {
+//
+//	int n;
+//	cin >> n;
+//
+//	arr[1] = 1;
+//	arr[2] = 1;
+//	arr[3] = 1;
+//	
+//	for (int i = 4; i <= n; i++) {
+//		arr[i] = arr[i - 1] + arr[i - 3];
+//	}
+//
+//	cout << arr[n];
+//
+//
+//}
