134b67105d150e4cb511b64abe0586e28870253c
YakultLover <rianbowgift@gmail.com>
2022-12-04 13:31:51 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_14581.cpp
@@ -0,0 +1,14 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	string n;
+//	cin >> n;
+//
+//
+//	cout << ":fan::fan::fan:\n";
+//	cout << ":fan::"+n+"::fan:\n";
+//	cout << ":fan::fan::fan:\n";
+//
+//}
