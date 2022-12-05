3acc72c8883a00b237b64a61bbb471e08b8c6cac
YakultLover <rianbowgift@gmail.com>
2022-12-05 09:24:17 +0900

    이중백터에 대한 연습문제가 많이 필요하다.

---
+++ Algorithm_Cpp/Programmers/PM_120842.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//#include <vector>
+//using namespace std;
+//
+//
+//vector<vector<int>> solution(vector<int> num_list, int n) {
+//    vector<vector<int>> answer;
+//
+//    int sum = 0;
+//    for (int i = 0; i < num_list.size()/n; i++) {
+//        answer.push_back({});
+//        for (int j = 0; j < n; j++) {
+//            answer[i].push_back(num_list[sum]);  
+//            sum++;
+//        }
+//       
+//    }
+//
+//
+//
+//    return answer;
+//}
+//
+//int main() {
+//
+//    int n = 2;
+//    vector<int> numlist = { 1, 2, 3, 4, 5, 6, 7, 8 };
+//
+//
+//
+//    solution(numlist,n);
+//
+//}
---
+++ Algorithm_Cpp/Programmers/PM_120905.cpp
@@ -0,0 +1,30 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//
+//vector<int> solution(int n, vector<int> numlist) {
+//    vector<int> answer;
+//
+//    for (int i = 0; i < numlist.size(); i++) {
+//        if (numlist[i] % n == 0) {
+//            answer.push_back(numlist[i]);
+//        }
+//    }
+//
+//
+//
+//
+//    return answer;
+//}
+//
+//int main() {
+//
+//    int n = 3;
+//    vector<int> numlist = { 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+//   
+//
+//    
+//    solution(n, numlist);
+//
+//}
