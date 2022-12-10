3384c1554d7b4c4c99e175a3feafed9a5e854373
YakultLover <rianbowgift@gmail.com>
2022-12-10 22:19:16 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level1/Base/PM_136798.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include<iostream>
+
+using namespace std;
+
+
+
+int solution(int number, int limit, int power) {
+    int answer = 0;
+
+    //1.10만이하의 소수구하기
+    //2.10만이하의 소수 사용하여 소인수분해하기
+    //3.
+
+
+
+    return answer;
+}
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+
+
+
+
+}
