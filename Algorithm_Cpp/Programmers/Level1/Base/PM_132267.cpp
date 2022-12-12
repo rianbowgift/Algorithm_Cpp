d0d78f598783c62d17c5e87880758e16d0c35415
YakultLover <rianbowgift@gmail.com>
2022-12-12 20:21:57 +0900

    백준의 달팽이문제와 유사. 구조를 잘 생각하고 짜야함

---
+++ Algorithm_Cpp/Programmers/Level1/Base/PM_132267.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(int a, int b, int n) {
+//    int answer = 0;
+//
+//    
+//    while (true) {
+//        int bins = n % a; //나누고 남은 빈병 수
+//        answer += (n / a) * b;
+//        n = ((n / a) * b) + bins;
+//        
+//        if (n < a) {
+//            break;
+//        }
+//    }
+//    
+//
+//
+//    return answer;
+//}
+//
+//
+//
