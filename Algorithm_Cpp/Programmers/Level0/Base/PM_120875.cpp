0ce991eb53870b3e8552ce1a48878d52d69fcb65
YakultLover <rianbowgift@gmail.com>
2022-12-13 23:39:49 +0900

    아주 비효율적이고 안좋은 코드. 나중에 다시짜볼것

--- Algorithm_Cpp/Programmers/Level0/Base/PM_120875.cpp
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_120875.cpp
@@ -4,48 +4,20 @@
 //
 //using namespace std;
 //
-//int gcd(int a, int b) {
-//    while (b != 0) {
-//        int r = a % b;
-//        a = b;
-//        b = r;
-//    }
-//    return a;
-//
-//}
 //
 //int solution(vector<vector<int>> dots) {
 //    int answer = 0;
 //
-//
-//
-//  
-//    
-//    if ((abs(dots[0][0] - dots[1][0]) == abs(dots[2][0] - dots[3][0])) &&
-//        abs(dots[0][1] - dots[1][1]) == abs(dots[2][1] - dots[3][1])) {
+//    if (((double)(dots[0][1] - dots[1][1]) / (double)(dots[0][0] - dots[1][0])) == ((double)(dots[2][1] - dots[3][1]) / (double)(dots[2][0] - dots[3][0]))) {
 //        
 //        answer++;
 //    }
-//    else if ((abs(dots[0][0] - dots[2][0]) == abs(dots[1][0] - dots[3][0])) &&
-//        abs(dots[0][1] - dots[2][1]) == abs(dots[1][1] - dots[3][1])) {
+//    else if (((double)(dots[0][1] - dots[2][1]) / (double)(dots[0][0] - dots[2][0])) == ((double)(dots[1][1] - dots[3][1]) / (double)(dots[1][0] - dots[3][0]))) {
 //        answer++;
 //    }
-//    else if ((abs(dots[0][0] - dots[3][0]) == abs(dots[1][0] - dots[2][0])) &&
-//        abs(dots[0][1] - dots[3][1]) == abs(dots[1][1] - dots[2][1])) {
+//    else if (((double)(dots[0][1] - dots[3][1]) / (double)(dots[0][0] - dots[3][0])) == ((double)(dots[1][1] - dots[2][1]) / (double)(dots[1][0] - dots[2][0]))) {
 //        answer++;
 //    }
-//
-//
-//
-//    //1.두개의 점을 몇번 이어봐야하는가
-//    //12 34   13 24    14 23 3번!
-//
-//
-//    //평행인지 어떻게 판단하는가
-//
-//
-//
-//
 //
 //
 //
---
+++ Algorithm_Cpp/Programmers/Level2/PM_92341.cpp
@@ -0,0 +1,16 @@
+//#include<iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<int> solution(vector<int> fees, vector<string> records) {
+//    vector<int> answer;
+//
+//
+//
+//
+//
+//
+//    return answer;
+//}
