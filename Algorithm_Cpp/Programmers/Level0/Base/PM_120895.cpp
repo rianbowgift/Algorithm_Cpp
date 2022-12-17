9a208a50893010b27193bba7897e90cbf0da859d
YakultLover <rianbowgift@gmail.com>
2022-12-17 13:58:22 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_120895.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//string solution(string my_string, int num1, int num2) {
+//    string answer = "";
+//
+//    for (int i = 0; i < my_string.length(); i++) {
+//        if (i == num1) {
+//            char temp = my_string[num1];
+//            my_string[num1] = my_string[num2];
+//            my_string[num2] = temp;
+//        }
+//    }
+//    answer = my_string;
+//
+//    return answer;
+//}
