3f2df770ec119b5a44b5a00c1f827574ab01a2db
YakultLover <rianbowgift@gmail.com>
2022-12-18 10:22:05 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_120899.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<int> solution(vector<int> array) {
+//    vector<int> answer;
+//
+//    int max = -1;
+//    int index = -1;
+//    for (int i = 0; i < array.size(); i++) {
+//        if (max < array[i]) {
+//            max = array[i];
+//            index = i;
+//        }
+//    }
+//    answer.push_back(max);
+//    answer.push_back(index);
+//
+//    return answer;
+//}
