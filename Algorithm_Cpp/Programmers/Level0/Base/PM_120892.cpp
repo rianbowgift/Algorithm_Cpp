f707e8c005effd766083269a1f5c0f5df8f2d9d4
YakultLover <rianbowgift@gmail.com>
2022-12-21 14:25:04 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_120892.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//string solution(string cipher, int code) {
+//    string answer = "";
+//
+//    for (int i = 0; i < cipher.length(); i++) {
+//        if ((i+1) % code == 0) {
+//            answer += cipher[i];
+//        }
+//    }
+//    
+//    return answer;
+//}
