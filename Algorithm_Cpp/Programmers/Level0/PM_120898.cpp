14e0248bc67be789e4cfa31c6522895c0643254c
YakultLover <rianbowgift@gmail.com>
2022-12-25 14:42:15 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/PM_120898.cpp
@@ -0,0 +1,14 @@
+//#include<iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(string message) {
+//    int answer = 0;
+//
+//    answer = message.length() * 2;
+//
+//
+//    return answer;
+//}
