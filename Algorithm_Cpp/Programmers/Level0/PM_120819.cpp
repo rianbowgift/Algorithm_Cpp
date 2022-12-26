f5d6d3e578815e8d79a0a5decdd0cabaa3351d66
rian <rianbowgift@gmail.com>
2022-12-26 14:17:47 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/PM_120819.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<int> solution(int money) {
+//    vector<int> answer;
+//
+//    if (money >= 5500) {
+//        answer.push_back(money / 5500);
+//        answer.push_back(money % 5500);
+//    }
+//    else {
+//        answer.push_back(0);
+//        answer.push_back(money % 5500);
+//    }
+//
+//    return answer;
+//}
