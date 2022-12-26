68081dfeb1b92bdf4cec23eb5e4ed7c48387df20
rian <rianbowgift@gmail.com>
2022-12-26 21:16:40 +0900

    cout.precision() 사용과 cout << fixed 사용법 유의

---
+++ Algorithm_Cpp/Baekjoon/BJ_14729.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//
+//
+//
+//int main() {
+//
+//	int n;
+//	cin >> n;
+//	vector<double> arr;
+//
+//	for (int i = 0; i < n; i++) {
+//		double temp;
+//		cin >> temp;
+//		arr.push_back(temp);
+//	}
+//
+//	cout << fixed;
+//	cout.precision(3);
+//
+//	sort(arr.begin(), arr.end());
+//	for (int i = 0; i < 7; i++) {
+//		cout << arr[i] << "\n";
+//	}
+//
+//
+//}
