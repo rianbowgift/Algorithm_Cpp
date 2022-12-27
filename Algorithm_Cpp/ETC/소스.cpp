11eba5b9b404dea8802648083757fc6f4e159c4d
rian <rianbowgift@gmail.com>
2022-12-27 14:03:35 +0900

    테스트

--- Algorithm_Cpp/ETC/소스.cpp
+++ Algorithm_Cpp/ETC/소스.cpp
@@ -1,29 +1,14 @@
-//#include<iostream>
-//using namespace std;
-//
-//
-//int arr[20];
-//int main() {
-//
-//	ios::sync_with_stdio(0);
-//	cin.tie(0);
-//
-//	int n;
-//
-//	cin >> n;
-//
-//	arr[1] = 1;
-//	arr[2] = 1;
-//	
-//
-//	for (int i = 3; i <= n; i++) {
-//		arr[i] = arr[i - 1] + arr[i - 2];
-//	}
-//
-//	cout << arr[n];
-//
-//
-//
-//
-//
-//}
+#include<iostream>
+using namespace std;
+
+
+int arr[20];
+int main() {
+
+	cout << "test";
+
+
+
+
+
+}
