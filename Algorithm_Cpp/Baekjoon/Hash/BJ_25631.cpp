d46a1c1c45800a548dc7f2f85edbba1e5336f66d
rian <rianbowgift@gmail.com>
2022-12-27 22:13:46 +0900

    로직을 빠르게 파악하는게 핵심

---
+++ Algorithm_Cpp/Baekjoon/Hash/BJ_25631.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//#include<algorithm>
+//#include<map>
+//using namespace std;
+//
+//int main() {
+//
+//
+//	map<int, int> mp;
+//	int n;
+//	cin >> n;
+//	int max = 0;
+//	for (int i = 0; i < n; i++) {
+//		int temp;
+//		cin >> temp;
+//		mp[temp]++;
+//		if (mp[temp] > max) {
+//			max = mp[temp];
+//		}
+//
+//	}
+//
+//	cout << max;
+//
+//
+//
+//}
