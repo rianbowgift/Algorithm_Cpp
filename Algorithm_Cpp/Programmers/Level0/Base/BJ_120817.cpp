423072fbddcab8df8fa31c9a551ef1cb94916e06
rian <rianbowgift@gmail.com>
2022-12-29 15:21:03 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/BJ_120817.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//double solution(vector<int> numbers) {
+//    double answer = 0;
+//
+//    for (int i = 0; i < numbers.size(); i++) {
+//        answer += numbers[i];
+//    }
+//
+//    cout << fixed;
+//    cout.precision(1);
+//    return answer / numbers.size();
+//
+//}
+//
