20b944bc18f20fad04438b08451a4a41103a88f0
YakultLover <rianbowgift@gmail.com>
2023-01-12 20:58:34 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_120813.cpp
@@ -0,0 +1,17 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<int> solution(int n) {
+//    vector<int> answer;
+//
+//    answer.push_back(1);
+//    for (int i = 2; i <= n; i++) {
+//        if (i % 2 != 0) {
+//            answer.push_back(i);
+//        }
+//    }
+//
+//    return answer;
+//}
