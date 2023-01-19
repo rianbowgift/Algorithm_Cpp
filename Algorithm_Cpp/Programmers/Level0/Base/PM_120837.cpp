73bae8796bb92c301cd15b5b61b292829c190d5e
YakultLover <rianbowgift@gmail.com>
2023-01-19 09:22:35 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_120837.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(int hp) {
+//    int answer = 0;
+//
+//    while (true) {
+//        if (hp == 0) {
+//            break;
+//        }
+//        if (hp >= 5) {
+//            answer++;
+//            hp -= 5;
+//            continue;
+//        }
+//        if (hp >= 3) {
+//            answer++;
+//            hp -= 3;
+//            continue;
+//        }
+//        answer++;
+//        hp -= 1;
+//    }
+//
+//    return answer;
+//}
