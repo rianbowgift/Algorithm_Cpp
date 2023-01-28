339bc6ab0b0f77393fea903328f19ce1f5384df2
YakultLover <rianbowgift@gmail.com>
2023-01-28 22:23:37 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_26209.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	for (int i = 0; i < 8; i++) {
+//		int temp;
+//		cin >> temp;
+//		if (temp == 0 || temp == 1) {
+//			continue;
+//		}
+//		else {
+//			cout << "F";
+//			return 0;
+//		}
+//	}
+//
+//	cout << "S";
+//	
+//
+//}
