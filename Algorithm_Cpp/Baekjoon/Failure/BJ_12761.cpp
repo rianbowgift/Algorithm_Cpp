427f360878192cc82464eaa2b62bc7c73d5a7b80
rian <rianbowgift@gmail.com>
2023-02-02 20:01:22 +0900

    일차원BFS. 어렵지않은데 모르겠다

---
+++ Algorithm_Cpp/Baekjoon/Failure/BJ_12761.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//#include<algorithm>
+//#include<queue>
+//using namespace std;
+//#define X first
+//#define Y second
+//int dist[1000002];
+//int n, k;
+//
+//int main() {
+//	
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cin >> n >> k;
+//
+//
+//
+//
+//	return 0;
+//}
