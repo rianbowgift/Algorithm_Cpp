ea2e62dd497605b28ec2d75a7487f1e923e9b673
rian <rianbowgift@gmail.com>
2023-02-02 20:04:34 +0900

    ADA로만 풀수있는 문제였다..

---
+++ Algorithm_Cpp/Baekjoon/Failure/BJ_2372.cpp
@@ -0,0 +1,14 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << "Animal      Count" << "\n";
+//	cout << "-----------------" << "\n";
+//	cout << "Chickens      100" << "\n";
+//	cout << "Clydesdales     5" << "\n";
+//	cout << "Cows           40" << "\n";
+//	cout << "Goats          22" << "\n";
+//	cout << "Steers          2";
+//
+//
+//}
