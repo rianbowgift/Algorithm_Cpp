ad41b646c11ac749eb22fa68ce603426eff58752
rian <rianbowgift@gmail.com>
2023-02-07 10:25:31 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_27433.cpp
@@ -0,0 +1,13 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	long a;
+//	cin >> a;
+//	long sum=1;
+//	for (int i = 1; i <=a; i++) {
+//		sum *= i;
+//	}
+//	cout << sum;
+//}
