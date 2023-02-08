b49102836b707be3e69d946c339d54d5ab609f5a
YakultLover <rianbowgift@gmail.com>
2023-02-08 09:27:29 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_26766.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//
+//	int a;
+//	cin >> a;
+//
+//	for (int i = 0; i < a; i++) {
+//cout << " @@@   @@@ " << "\n";
+//cout << "@   @ @   @" << "\n";
+//cout << "@    @    @" << "\n";
+//cout << "@         @" << "\n";
+//cout << " @       @ " << "\n";
+//cout << "  @     @  " << "\n";
+//cout << "   @   @   " << "\n";
+//cout << "    @ @    " << "\n";
+//cout << "     @     " << "\n";
+//
+//
+//	}
+//
+//
+//}
