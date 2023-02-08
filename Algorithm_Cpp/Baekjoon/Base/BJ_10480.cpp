ecc42dc8bf6767de2c507c8da14d04e3f71afbd7
YakultLover <rianbowgift@gmail.com>
2023-02-09 02:28:03 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_10480.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int a;
+//	cin >> a;
+//	for (int i = 0; i < a; i++) {
+//		int temp;
+//		cin >> temp;
+//		if (abs(temp) % 2 == 0) {
+//			cout << temp << " is even" << "\n";
+//		}
+//		else {
+//			cout << temp << " is odd" << "\n";
+//		}
+//
+//	}
+//
+//
+//}
