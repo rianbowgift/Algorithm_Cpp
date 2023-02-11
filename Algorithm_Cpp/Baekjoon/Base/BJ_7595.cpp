97a69a734e85f9150942b45f62333feb9c9d16e3
rian <rianbowgift@gmail.com>
2023-02-11 12:08:37 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_7595.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//
+//	while(true) {
+//		int temp;
+//		cin >> temp;
+//		if (temp == 0) {
+//			break; 
+//		}
+//
+//		for (int i = 1; i <= temp; i++) {
+//			for (int j = 0; j < i; j++) {
+//				cout << "*";
+//			}
+//			cout << "\n";
+//		}
+//		
+//
+//
+//
+//	}
+//}
