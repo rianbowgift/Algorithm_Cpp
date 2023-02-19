bb45833d29273d1f82e3217d9d583e5564af56bd
rian <rianbowgift@gmail.com>
2023-02-19 17:38:21 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_6840.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//
+//int main() {
+//
+//	int arr[3];
+//
+//	for (int i = 0; i < 3; i++) {
+//		int temp;
+//		cin >> temp;
+//		arr[i] = temp;
+//	}
+//	sort(arr,arr+3);
+//	cout << arr[1];
+//
+//
+//
+//}
