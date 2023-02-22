c435d9cb51f5e9708fa250952969e8b3dc9a1fcd
rian <rianbowgift@gmail.com>
2023-02-22 17:58:07 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_5524.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int n;
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		string temp;
+//		cin >> temp;
+//		string word = "";
+//		for (int j = 0; j < temp.length(); j++) {
+//			if (temp[j] <= 'Z') {
+//				word += temp[j] + 32;
+//			}
+//			else {
+//				word += temp[j];
+//			}
+//		}
+//		cout << word << "\n";
+//	 }
+//
+//
+//}
