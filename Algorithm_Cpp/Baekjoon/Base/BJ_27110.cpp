ed4661dd7cf7517c888c59cab5002ca7e67f94e0
YakultLover <rianbowgift@gmail.com>
2023-02-24 23:02:52 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_27110.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int x[5];
+//	int sum = 0;
+//	cin >> x[0] >> x[1] >> x[2] >> x[3];
+//
+//	if (x[1] <= x[0]) {
+//		sum += x[1];
+//	}
+//	else {
+//		sum += x[0];
+//	}
+//	if (x[2] <= x[0]) {
+//		sum += x[2];
+//	}
+//	else {
+//		sum += x[0];
+//	}
+//
+//	if (x[3] <= x[0]) {
+//		sum += x[3];
+//	}
+//	else {
+//		sum += x[0];
+//	}
+//
+//
+//
+//	cout << sum;
+//}
