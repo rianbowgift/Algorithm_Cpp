89387faa6f4989e4afbd3820ee89f15279878cb8
rian <rianbowgift@gmail.com>
2023-03-10 12:35:17 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_10188.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int x;
+//	cin >> x;
+//	for (int q = 0; q < x; q++) {
+//		int temp1, temp2;
+//		cin >> temp1 >> temp2;
+//
+//		for (int i = 0; i < temp2; i++) {
+//			for (int j = 0; j < temp1; j++) {
+//				cout << "X";
+//			}
+//			cout << "\n";
+//		}
+//
+//		cout << "\n";
+//	}
+//
+//
+//}
