d4651556c9ae931d5c8bdb05d4b7b388201ab539
YakultLover <rianbowgift@gmail.com>
2023-03-16 00:15:18 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_13985.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main() {
+//	int sum = 0;
+//	string x;
+//	getline(cin, x);
+//
+//	if (x[2] == '+') {
+//		sum = (x[0] - '0') + x[4] - '0';
+//
+//		if (x[8] - '0' == sum) {
+//			cout << "YES";
+//		}
+//		else {
+//			cout << "NO";
+//		}
+//	}
+//
+//}
