fe13283a41f0e051ebb37f90379428665274aeaf
rian <rianbowgift@gmail.com>
2023-03-20 18:17:34 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_2083.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	while (true) {
+//
+//		string a;
+//		int b, c;
+//
+//		cin >> a >> b >> c;
+//
+//		if (a == "#" && b == 0 && c == 0) {
+//			break;
+//		}
+//
+//		if (b > 17 || c >= 80) {
+//			cout << a << " Senior\n";
+//		}
+//		else {
+//			cout << a << " Junior\n";
+//		}
+//
+//	}
+//
+//}
