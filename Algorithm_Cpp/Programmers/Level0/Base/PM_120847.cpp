67bea124b9fc16bc671690f23e43d96000b5c7d3
YakultLover <rianbowgift@gmail.com>
2023-03-22 07:35:27 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_120847.cpp
@@ -0,0 +1,16 @@
+//#include<iostream>
+//#include <string>
+//#include <vector>
+//#include<algorithm>
+//
+//using namespace std;
+//
+//int solution(vector<int> numbers) {
+//    int answer = 0;
+//
+//    sort(numbers.begin(), numbers.end());
+//    int size = numbers.size();
+//    answer = numbers[size - 1] * numbers[size - 2];
+//
+//    return answer;
+//}
