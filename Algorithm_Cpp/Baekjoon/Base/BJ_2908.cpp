b31d45e7a9d328d63e72de334270703eb54bc3a2
YakultLover <rianbowgift@gmail.com>
2023-03-23 11:34:28 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_2908.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//#include<string>
+//#include<algorithm>
+//using namespace std;
+//
+//int main() {
+//	string a, b;
+//	cin >> a >> b;
+//
+//	reverse(a.begin(), a.end());
+//	reverse(b.begin(), b.end());
+//	int c = stoi(a);
+//	int d = stoi(b);
+//	cout << max(c, d);
+////
+//
+//}
