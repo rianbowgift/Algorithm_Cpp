b5a8081010e59451106e1fb8dbb1f6999bbb930c
YakultLover <rianbowgift@gmail.com>
2023-03-25 17:26:32 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_5789.cpp
@@ -0,0 +1,40 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//
+//
+//	int count;
+//	cin >> count;
+//
+//	for (int i = 0; i < count; i++) {
+//		string temp;
+//		cin >> temp;
+//		bool status = false;
+//		int x = 0;
+//		int y = temp.size() - 1;
+//
+//		while (true) {
+//			if (temp[x] == temp[y]) {
+//				status = true;
+//			}
+//			else {
+//				status = false;
+//			}
+//			x++;
+//			y--;
+//			if (x > y) {
+//				if (status == true) {
+//					cout << "Do-it\n";
+//				}
+//				else {
+//					cout << "Do-it-Not\n";
+//				}
+//				break;
+//			}
+//
+//		}
+//		
+//
+//
+//	}
+//}
