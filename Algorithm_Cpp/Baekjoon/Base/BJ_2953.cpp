d5eebd48e36b93da570a6f5835f9767b9eedb37f
YakultLover <rianbowgift@gmail.com>
2023-03-28 12:55:09 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_2953.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//
+//	int man_num = 0;
+//	int man_point = 0;
+//
+//	for (int i = 0; i < 5; i++) {
+//		int a, b, c, d;
+//		cin >> a >> b >> c >> d;
+//		if (a + b + c + d > man_point) {
+//			man_point = a + b + c + d;
+//			man_num = i + 1;
+//		}
+//	}
+//
+//	cout << man_num << "\n" << man_point;
+//
+//
+//}
