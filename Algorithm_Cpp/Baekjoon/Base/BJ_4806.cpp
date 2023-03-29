eddbc0a92131abed5940fc4e8afd3eae8ff70862
rian <rianbowgift@gmail.com>
2023-03-29 14:24:24 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_4806.cpp
@@ -0,0 +1,15 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main() {
+//	string str;
+//	int sum = 0;
+//	while (getline(cin, str)) {
+//		
+//		sum++;
+//	}
+//	cout << sum;
+//
+//
+//}
