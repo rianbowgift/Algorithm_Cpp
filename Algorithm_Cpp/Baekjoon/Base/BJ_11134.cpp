42ce3b3f6a40a5509b642a454091669a114b1b85
rian <rianbowgift@gmail.com>
2023-03-30 10:27:33 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_11134.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int n;
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		int max, c;
+//		cin >> max >> c;
+//
+//		if (max % c == 0) {
+//			cout << max / c;
+//		}
+//		else {
+//			cout << (max / c) + 1;
+//		}
+//		cout << "\n";
+//	}
+//
+//
+//}
