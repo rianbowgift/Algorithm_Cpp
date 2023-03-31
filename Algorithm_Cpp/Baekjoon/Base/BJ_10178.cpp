f649def5058b724f3bf62941576c041c1d962054
YakultLover <rianbowgift@gmail.com>
2023-03-31 15:00:25 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_10178.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int n;
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		int a, b;
+//		int x, y;
+//		cin >> a >> b;
+//
+//		x = a / b;
+//		y = a % b;
+//
+//		cout << "You get ";
+//		cout << x;
+//		cout << " piece(s) and your dad gets ";
+//		cout << y;
+//		cout << " piece(s).\n";
+//
+//
+//	}
+//
+//
+//
+//}
