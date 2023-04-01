fa009a8cb2bced29db9dfed70e32b16cc2a2a62d
YakultLover <rianbowgift@gmail.com>
2023-04-01 11:58:52 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_10214.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	int n;
+//	cin >> n;
+//	int a_sum=0;
+//	int b_sum = 0;
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < 9; j++) {
+//
+//
+//			int a, b;
+//			cin >> a >> b;
+//			a_sum += a;
+//			b_sum += b;
+//
+//		}
+//		if (a_sum > b_sum) {
+//			cout << "Yonsei";
+//		}
+//		else if (a_sum < b_sum) {
+//			cout << "Korea";
+//		}
+//		else {
+//			cout << "Draw";
+//		}
+//		cout << "\n";
+//	}
+//	a_sum = 0;
+//	b_sum = 0;
+//}
