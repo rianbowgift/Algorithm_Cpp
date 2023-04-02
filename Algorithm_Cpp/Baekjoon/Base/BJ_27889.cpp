62cbc4bd80ca45a48c5ebfaad56e47fea32a39c2
YakultLover <rianbowgift@gmail.com>
2023-04-03 01:27:51 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_27889.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	string x;
+//	cin >> x;
+//
+//	if (x == "NLCS") {
+//		cout << "North London Collegiate School";
+//	}
+//	else if (x == "BHA") {
+//		cout << "Branksome Hall Asia";
+//	}
+//	else if (x == "KIS") {
+//		cout << "Korea International School";
+//	}
+//	else {;
+//	cout << "St. Johnsbury Academy";
+//	}
+//
+//}
