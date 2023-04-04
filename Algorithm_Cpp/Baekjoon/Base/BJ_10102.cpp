c1e1a724e53d90dfc584e641a84f59befea43132
YakultLover <rianbowgift@gmail.com>
2023-04-04 09:22:20 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_10102.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	int x;
+//	cin >> x;
+//	string n;
+//	cin >> n;
+//
+//	int a = 0;
+//	int b = 0;
+//	
+//	for (int i = 0; i < n.length(); i++) {
+//		if (n[i] == 'A') {
+//			a++;
+//		}
+//		else {
+//			b++;
+//		}
+//	}
+//	if (a < b) {
+//		cout << "B";
+//	}
+//	if (a > b) {
+//		cout << "A";
+//	}
+//	if (a == b) {
+//		cout << "Tie";
+//	}
+//
+//
+//}
