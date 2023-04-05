4494e4c32ad2349a76d268c42a573052e8d5061f
YakultLover <rianbowgift@gmail.com>
2023-04-06 00:04:07 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_23303.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main() {
+//	string x;
+//	getline(cin, x);
+//	
+//	if (x.find("D2") != std::string::npos || x.find("d2") != std::string::npos) {
+//		cout << "D2";
+//	}
+//	else {
+//		cout << "unrated";
+//	}
+//
+//
+//}
