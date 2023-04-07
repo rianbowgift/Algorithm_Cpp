b949abbe6004ae72472dc24a2922920a41fe8cfb
YakultLover <rianbowgift@gmail.com>
2023-04-07 14:02:27 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_4458.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main() {
+//	int n;
+//	cin >> n;
+//	cin.ignore();
+//	for (int i = 0; i < n; i++) {
+//		string x;
+//		getline(cin, x);
+//		bool sw = false;
+//		for (int j = 0; j < x.length(); j++) {
+//			if (j == 0) {
+//				x[0] = toupper(x[0]);
+//				cout << x[j];
+//				continue;
+//			}
+//			cout << x[j];
+//			
+//
+//		}
+//		cout << "\n";
+//
+//	}
+//
+//
+//
+//}
