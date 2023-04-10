0161a3c3def461d443c060fbac52e133bdd7c69d
YakultLover <rianbowgift@gmail.com>
2023-04-11 00:14:01 +0900

    간단히

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_3047.cpp
@@ -0,0 +1,38 @@
+//#include<iostream>
+//#include<algorithm>
+//#include<vector>
+//using namespace std;
+//
+//int main() {
+//
+//	vector<pair<int, char>> arr;
+//
+//	for (int i = 0; i < 3; i++) {
+//		int temp;
+//		cin >> temp;
+//		arr.push_back({temp, ' '});
+//	}
+//	sort(arr.begin(), arr.end());
+//	arr[0].second = 'A';
+//	arr[1].second = 'B';
+//	arr[2].second = 'C';
+//
+//	string st;
+//	cin >> st;
+//	for (int i = 0; i < 3; i++) {
+//		char temp = st[i];
+//		for (int j = 0; j < 3; j++) {
+//			if (temp == arr[j].second) {
+//				cout << arr[j].first << " ";
+//				break;
+//			}
+//		}
+//	}
+//
+//
+//
+//
+//
+//
+//
+//}
