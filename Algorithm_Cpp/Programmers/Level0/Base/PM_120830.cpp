aa9a893a7f69d21c46c8fb2451295618776fd29f
YakultLover <rianbowgift@gmail.com>
2023-04-14 18:15:34 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_120830.cpp
@@ -0,0 +1,28 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(int n, int k) {
+//    int answer = 0;
+//
+//    answer += n * 12000;
+//    answer += k * 2000;
+//
+//    while (true) {
+//        if (k <= 0 || n<10) {
+//            break;
+//        }
+//        if (n >= 10) {
+//            n -= 10;
+//            if (k > 0) {
+//                k--;
+//                answer -= 2000;
+//            }
+//        }
+//        
+//
+//    }
+//
+//    return answer;
+//}
