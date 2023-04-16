766a3fce0f2b2a49a3b6ea90b5d04971c1aa31b3
YakultLover <rianbowgift@gmail.com>
2023-04-16 13:54:39 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_120815.cpp
@@ -0,0 +1,26 @@
+//#include <string>
+//#include <vector>
+//#include<iostream>
+//
+//using namespace std;
+//
+//int solution(int n) {
+//    int answer = 0;
+//    int point = 0;
+//    
+//    while (true) {
+//        if (point % n == 0 && point!=0) {
+//            break;
+//        }
+//        else {
+//            answer++;
+//            point += 6;
+//        }
+//
+//    }
+//
+//
+//    return answer;
+//}
+//
+//
