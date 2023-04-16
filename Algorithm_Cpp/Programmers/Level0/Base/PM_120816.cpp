6c5a7ae9815ed98269d014476e7b55b46687d8eb
YakultLover <rianbowgift@gmail.com>
2023-04-17 02:04:29 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_120816.cpp
@@ -0,0 +1,34 @@
+//#include <string>
+//#include <vector>
+//#include<iostream>
+//
+//using namespace std;
+//
+//int solution(int slice,int n) {
+//    int answer = 0;
+//    int temp = slice;
+//    
+//    while (true) {
+//        if (slice < n) {
+//            slice += temp;
+//            answer++;
+//            continue;
+//        }
+//            if (slice == n) {
+//                answer++;
+//                break;
+//            }
+//            else if(slice > n) {
+//                answer++;
+//                break;
+//            }
+//  
+//        
+//
+//    }
+//
+//
+//    return answer;
+//}
+//
+//
