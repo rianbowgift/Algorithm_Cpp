31f107f724e01f83b88748250dd08997fbe75367
YakultLover <rianbowgift@gmail.com>
2023-04-20 00:04:09 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_120906.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(int n) {
+//    int answer = 0;
+//    
+//    string temp = to_string(n);
+//
+//    for (int i = 0; i < temp.length(); i++) {
+//       
+//        answer += temp[i] - '0';
+//    }
+//
+//
+//    return answer;
+//}
