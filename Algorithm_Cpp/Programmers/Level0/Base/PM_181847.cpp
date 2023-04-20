80a5ce52de9d3f4a57aa9e5e6d6fe415acce9c3c
YakultLover <rianbowgift@gmail.com>
2023-04-21 02:46:47 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_181847.cpp
@@ -0,0 +1,27 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//string solution(string n_str) {
+//    string answer = "";
+//    if (n_str[0] == '0') {
+//        bool sw = false;
+//        for (int i = 0; i < n_str.length(); i++) {
+//            if (sw == true) {
+//                answer += n_str[i];
+//            }
+//            if (n_str[i] != '0' && sw == false) {
+//                sw = true;
+//                answer += n_str[i];
+//            }
+//        
+//        }
+//    }
+//    else {
+//        answer = n_str;
+//    }
+//
+//
+//    return answer;
+//}
