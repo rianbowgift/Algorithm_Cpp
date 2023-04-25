18ada388379c94d5c45bdc791954af8ba444ec33
YakultLover <rianbowgift@gmail.com>
2023-04-26 00:07:53 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_181949.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main(void) {
+//    string str;
+//    cin >> str;
+//    string dd;
+//    for (int i = 0; i < str.length(); i++) {
+//        if (str[i] >= 65 && str[i] <= 90) {
+//            dd+= str[i] + 32 ;
+//        }
+//        else {
+//            dd+= str[i] - 32;
+//        }
+//    }
+//    cout << dd;
+//    return 0;
+//}
