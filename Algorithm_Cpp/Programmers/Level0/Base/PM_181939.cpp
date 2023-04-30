f3c8a6d3d03be19b96729b0771b6202d829c51f5
YakultLover <rianbowgift@gmail.com>
2023-04-30 16:06:10 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_181939.cpp
@@ -0,0 +1,20 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(int a, int b) {
+//    int answer = 0;
+//    string x, y;
+//
+//    x = to_string(a) + to_string(b);
+//    y = to_string(b) + to_string(a);
+//    if (stoi(x) > stoi(y)) {
+//        answer = stoi(x);
+//    }
+//    else {
+//        answer = stoi(y);
+//    }
+//
+//    return answer;
+//}
