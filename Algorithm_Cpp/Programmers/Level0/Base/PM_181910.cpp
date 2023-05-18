c4335aeef35637b3f17d4324a43289e16781ecfd
YakultLover <rianbowgift@gmail.com>
2023-05-18 11:29:48 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_181910.cpp
@@ -0,0 +1,16 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//string solution(string my_string, int n) {
+//    string answer = "";
+//
+//    for (int i = 0; i < my_string.length(); i++) {
+//        if (my_string.length() - n <= i) {
+//            answer += my_string[i];
+//        }
+//    }
+//
+//    return answer;
+//}
