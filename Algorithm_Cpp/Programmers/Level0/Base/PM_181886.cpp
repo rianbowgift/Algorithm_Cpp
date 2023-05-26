293bd43487091749ea603d967b4c023671d6ee0a
YakultLover <rianbowgift@gmail.com>
2023-05-27 08:20:40 +0900

    간단히

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_181886.cpp
@@ -0,0 +1,19 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<string> solution(vector<string> names) {
+//    vector<string> answer;
+//    int count = 4;
+//    for (int i = 0; i < names.size(); i++) {
+//        if (count != 4) {
+//            count++;
+//        }
+//        else {
+//            answer.push_back(names[i]);
+//            count = 0;
+//        }
+//    }
+//    return answer;
+//}
