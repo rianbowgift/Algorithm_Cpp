193b2f86ad5e75630fc3e3ae2f7e4ccc5f0ce5d2
YakultLover <rianbowgift@gmail.com>
2023-06-15 00:35:40 +0900

    간단한 수학문제

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_181914.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string number) {
+    int answer = 0;
+    int sum = 0;
+    for (int i = 0; i < number.length(); i++) {
+        sum += number[i] - '0';
+    }
+    answer = sum % 9;
+
+    return answer;
+}
