4012414ed00e0717cddde7745f14c2a5d3bd7b0f
YakultLover <rianbowgift@gmail.com>
2023-06-19 00:49:23 +0900

    약간 햇갈리게만들어둔 간단한 연산

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_181854.cpp
@@ -0,0 +1,31 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<int> solution(vector<int> arr, int n) {
+//    vector<int> answer;
+//    if (arr.size() % 2 == 0) {
+//        for (int i = 0; i < arr.size(); i++) {
+//            if ((i + 1) % 2 == 0) {
+//                answer.push_back(arr[i]+n);
+//            }
+//            else {
+//                answer.push_back(arr[i] );
+//            }
+//        }
+//    
+//    }
+//    else {
+//        for (int i = 0; i < arr.size(); i++) {
+//            if ((i + 1) % 2 == 0) {
+//                answer.push_back(arr[i]);
+//            }
+//            else {
+//                answer.push_back(arr[i] + n);
+//            }
+//        }
+//    }
+//    
+//    return answer;
+//}
