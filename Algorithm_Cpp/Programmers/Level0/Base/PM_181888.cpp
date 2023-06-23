5a37c1f1562db44f2cf07563b81565a7d1796894
YakultLover <rianbowgift@gmail.com>
2023-06-24 00:25:00 +0900

    간단한 조건문

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_181888.cpp
@@ -0,0 +1,20 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<int> solution(vector<int> num_list, int n) {
+//    vector<int> answer;
+//    int temp = 0;
+//    for (int i = 0; i < num_list.size(); i++) {
+//        if (temp == 0) {
+//            answer.push_back(num_list[i]);
+//            temp = n-1;
+//        }
+//        else {
+//            temp--;
+//        }
+//        
+//    }
+//    return answer;
+//}
