f8387b7b625b150a7d351fac91bb6e9492d573f0
YakultLover <rianbowgift@gmail.com>
2023-06-29 00:08:11 +0900

    문제를 잘못이해. 좀더 재대로 읽을필요성이 있음

--- Algorithm_Cpp/Programmers/Level0/Base/PM_181841.cpp
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_181841.cpp
@@ -1,38 +1,31 @@
-//#include <string>
-//#include <vector>
-//#include <iostream>
-//using namespace std;
-//
-//string solution(vector<string> str_list, string ex) {
-//    string answer = "";
-//    for (int i = 0; i < str_list.size(); i++) {
-//        bool sw = true;
-//        int temp = 0;
-//        for (int j = str_list[i].length() - ex.length(); j<str_list[i].length(); j++) {
-//            if (str_list[i].length() < ex.length()) {
-//                sw = false;
-//                break;
-//            }
-//            if (str_list[i][j] != ex[temp]) {
-//                //cout << str_list[i][j] << " " << ex[temp] << "\n";
-//                sw = false;
-//                break;
-//            }
-//            else {
-//                temp++;
-//            }
-//        }
-//        if (sw == false) {
-//            answer += str_list[i];
-//        }
-//    }
-//    return answer;
-//}
-//
-//int main() {
-//    vector<string> str_list = { "abc", "def", "ghi" };
-//
-//    string ex = "ef";
-//    solution(str_list, ex);
-//
-//}
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(vector<string> str_list, string ex) {
+    string answer = "";
+    for (int i = 0; i < str_list.size(); i++) {
+        bool sw = true;
+        int temp = 0;
+        int count = ex.length();
+        for (int j = 0; j < str_list[i].length(); j++) {
+            if (str_list[i][j] == ex[temp]) {
+                count--;
+                temp++;
+            }
+            else {
+                temp = 0;
+            }
+            if(count==0){
+                sw = false;
+                break;
+            }
+        }
+        if (sw == true) {
+            answer += str_list[i];
+        }
+    }
+    return answer;
+}
+
