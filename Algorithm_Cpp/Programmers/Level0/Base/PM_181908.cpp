e35675daa55cfaff0ec6f97fc165631ac2cf4c9a
YakultLover <rianbowgift@gmail.com>
2023-07-01 12:50:17 +0900

    간단한 조건검색

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_181908.cpp
@@ -0,0 +1,19 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(string my_string, string is_suffix) {
+//    int answer = 0;
+//    for (int i = 0; i < my_string.length(); i++) {
+//        string temp = "";
+//        for (int j = i; j < my_string.length(); j++) {
+//            temp += my_string[j];
+//        }
+//        if (temp == is_suffix) {
+//            answer = 1;
+//            break;
+//        }
+//    }
+//    return answer;
+//}
