a51b5a0569b73e04eaaf7838ce59e106d1b29f22
YakultLover <rianbowgift@gmail.com>
2023-07-02 00:01:23 +0900

    간단한 이프검사

---
+++ Algorithm_Cpp/Programmers/Level0/Base/PM_181906.cpp
@@ -0,0 +1,17 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(string my_string, string is_suffix) {
+//    int answer = 0;
+//    string temp = "";
+//    for (int i = 0; i < my_string.length(); i++) {
+//        temp += my_string[i];
+//        if (temp == is_suffix) {
+//            answer = 1;
+//            break;
+//        }
+//    }
+//    return answer;
+//}
