31c9a810da122036e2573dbe51c94ee6cc498be5
YakultLover <rianbowgift@gmail.com>
2023-07-14 06:44:00 +0900

    간단한 계산문

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_27959.cpp
@@ -0,0 +1,19 @@
+//#include <string>
+//#include <vector>
+//#include<iostream>
+//
+//using namespace std;
+//
+//
+//int main() {
+//    long a, b;
+//    cin >> a >> b;
+//    long money = a * 100;
+//    if (money >= b) {
+//        cout << "Yes";
+//    }
+//    else {
+//        cout << "No";
+//    }
+//
+//}
