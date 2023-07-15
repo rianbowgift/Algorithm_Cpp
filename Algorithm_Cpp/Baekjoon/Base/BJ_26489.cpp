698599b5a7b55bde5e02752a42f1aa3863631479
YakultLover <rianbowgift@gmail.com>
2023-07-16 00:03:41 +0900

    무한입력예시

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_26489.cpp
@@ -0,0 +1,16 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//int main() {
+//    int sum = 0;
+//    while (!cin.eof()) {
+//        string x;
+//        getline(cin, x);
+//        if (x == "gum gum for jay jay") {
+//            sum++;
+//        }
+//    }
+//    cout << sum;
+//
+//}
