da99ca5c436e1893b689ae0357c0937dafbb746a
YakultLover <rianbowgift@gmail.com>
2023-07-15 09:37:29 +0900

    간단한 조건문

---
+++ Algorithm_Cpp/Baekjoon/Base/BJ_28235.cpp
@@ -0,0 +1,25 @@
+//#include <string>
+//#include <vector>
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//
+//    string a;
+//    cin >> a;
+//    if (a == "SONGDO") {
+//        cout << "HIGHSCHOOL";
+//    }
+//    else if (a == "CODE") {
+//        cout << "MASTER";
+//    }
+//    else if (a == "2023") {
+//        cout << "0611";
+//    }
+//    else {
+//        cout << "CONTEST";
+//    }
+//
+//
+//}
+//
